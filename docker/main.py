f620fe7b503724c99989e0cf436d6e6b0ba91396
trillville <tillman.elser@sentry.io>
2022-01-25 14:41:30 -0800

    remove kats dependencies, update output format

--- docker/main.py
+++ docker/main.py
@@ -1,37 +1,13 @@
 import logging
-
-from prophet_model import ProphetModel, ProphetParams
-from kats.detectors.cusum_detection import CUSUMDetector
-from kats.consts import TimeSeriesData
+import time
 
 from flask import Flask, request
-from datetime import datetime
 import pandas as pd
+
+from prophet_detector import ProphetDetector, ProphetParams
 
 log = logging.getLogger()
 app = Flask(__name__)
-
-import time
-
-
-@app.route("/changepoint", endpoint="changepoint", methods=["POST"])
-def changepoint():
-    s = time.time()
-    data = request.get_json()
-
-    train = pd.DataFrame()
-    train["y"] = data["train"]["y"]
-    train["ds"] = data["train"]["ds"]
-    train["ds"] = pd.to_datetime(train["ds"], format="%Y-%m-%d %H:%M:%S")
-
-    tsd = TimeSeriesData(train, time_col_name="ds")
-
-    detector = CUSUMDetector(tsd)
-    change_points = detector.detector(change_directions=["increase"])
-
-    cp, metadata = change_points[0]
-
-    return dict(change_start=datetime.strftime(cp._start_time, format="%Y-%m-%d %H:%M:%S"))
 
 
 @app.route("/predict", endpoint="predict", methods=["POST"])
@@ -46,10 +22,10 @@ def predict():
         changepoint_prior_scale=0.01,
         weekly_seasonality=14,
         daily_seasonality=False,
-        uncertainty_samples=None
+        uncertainty_samples=None,
     )
-    m = ProphetModel(pd.DataFrame(data["train"]), start, end, params)
-
+    m = ProphetDetector(pd.DataFrame(data["train"]), start, end, params)
+    m.pre_process_data()
     timings["pre_process"] = time.time() - s
 
     s = time.time()
@@ -57,14 +33,67 @@ def predict():
     timings["train"] = time.time() - s
 
     s = time.time()
-    fcst = m.gen_forecast()
+    fcst = m.predict()
     timings["predict"] = time.time() - s
-    s = time.time()
-    m.add_prophet_uncertainty(fcst, using_train_df=True)
-    timings["uncertainty"] = time.time() - s
-    s = time.time()
-    fcst = m.gen_scores(fcst)
-    timings["gen_scores"] = time.time() - s
-    print(timings)
 
-    return fcst[["ds", "y", "yhat", "yhat_lower", "yhat_upper", "score", "scaled_score", "final_score"]].to_json()
+    s = time.time()
+    m.add_prophet_uncertainty(fcst)
+    timings["uncertainty"] = time.time() - s
+
+    s = time.time()
+    fcst = m.scale_scores(fcst)
+    fcst["ds"] = (fcst["ds"].astype(np.int64) * 1e-9).astype(np.int64)
+    timings["gen_scores"] = time.time() - s
+
+    s = time.time()
+    output = process_output(fcst)
+    timings["format_output"] = time.time() - s
+
+    logging.info(timings)
+
+    return output
+
+
+def convert_ts(ts, value_col):
+    data = zip(
+        list(ts["ds"]),
+        [[{"count": x}] for x in list(ts[value_col])],
+    )
+    start = ts["ds"].iloc[0]
+    end = ts["ds"].iloc[-1]
+    return {"data": list(data), "start": start, "end": end}
+
+
+def aggregate_anomalies(data):
+    anomalies = []
+    last_score = None
+    anomaly_index = -1
+    granularity = 5 * 60  # TODO get from class
+    for ds_time, score in data[~data["anomalies"].isna()][["ds", "anomalies"]].itertuples(index=False):
+        if score == last_score:
+            anomalies[anomaly_index]["end"] = ds_time + granularity
+        else:
+            anomaly_index += 1
+            anomalies.append(
+                {
+                    "start": ds_time,
+                    "end": ds_time + granularity,
+                    "confidence": score,
+                    "id": anomaly_index,
+                }
+            )
+    last_score = score
+
+    return anomalies
+
+
+def process_output(data):
+    results = {
+        "y": convert_ts(data, "y"),
+        "yhat_upper": convert_ts(data, "yhat_upper"),
+        "yhat_lower": convert_ts(data, "yhat_lower"),
+        # TODO: remove (this is just for debugging)
+        "anomaly_scores": convert_ts(data, "final_score"),
+        "anomalies": aggregate_anomalies(data),
+    }
+    return results
