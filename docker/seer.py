7a7f15a6b05b68e6ae1524b3b649e170ce159b44
trillville <tillman.elser@sentry.io>
2022-02-08 17:05:02 -0800

    many small fixes/tweaks

--- docker/seer.py
+++ docker/seer.py
@@ -18,7 +18,7 @@ sentry_sdk.init(
 app = Flask(__name__)
 
 MODEL_PARAMS = ProphetParams(
-    interval_width=0.95,
+    interval_width=0.975,
     changepoint_prior_scale=0.01,
     weekly_seasonality=14,
     daily_seasonality=False,
@@ -49,7 +49,7 @@ def predict():
     with sentry_sdk.start_span(
         op="data.preprocess", description="Preprocess data to prepare for anomaly detection"
     ) as span:
-        detector.pre_process_data(pd.DataFrame(data["data"]), start, end)
+        detector.pre_process_data(pd.DataFrame(data["data"]), granularity, start, end)
 
     with sentry_sdk.start_span(
         op="model.train", description="Train forecasting model"
@@ -68,6 +68,7 @@ def predict():
         op="data.anomaly.scores", description="Generate anomaly scores using forecast"
     ) as span:
         fcst = detector.scale_scores(fcst)
+        fcst = fcst[start:end]
 
     with sentry_sdk.start_span(
         op="data.format", description="Format data for frontend"
@@ -107,15 +108,21 @@ def aggregate_anomalies(data, granularity):
         expected: expected count for metric (from yhat)
         id: id/label for each anomaly
     """
+    score_map = {
+        1: "low",
+        2: "high"
+    }
+    score_lookup = {v: k for k, v in score_map.items()}
     anomalies = []
-    last_score = None
+    last_score, last_time = None, None
     anomaly_index = -1
     sum_expected, sum_actual = 0, 0
     for ds_time, score, y, yhat in data.itertuples(index=False):
-        if score == last_score:
+        if last_time and ds_time <= last_time + (granularity * 3):
             anomalies[anomaly_index]["end"] = int(ds_time + granularity)
             anomalies[anomaly_index]["received"] += round(y, 5)
             anomalies[anomaly_index]["expected"] += round(yhat, 5)
+            anomalies[anomaly_index]["confidence"] = score_map[max(score, score_lookup[anomalies[anomaly_index]["confidence"]])]
         else:
             sum_expected = yhat
             sum_actua = y
@@ -124,13 +131,14 @@ def aggregate_anomalies(data, granularity):
                 {
                     "start": int(ds_time),
                     "end": int(ds_time + granularity),
-                    "confidence": score,
+                    "confidence": score_map[score],
                     "received": round(y, 5),
                     "expected": round(yhat, 5),
                     "id": anomaly_index,
                 }
             )
         last_score = score
+        last_time = ds_time
 
     return anomalies
 
@@ -173,5 +181,8 @@ def process_output(data, granularity):
         "yhat_upper": convert_ts(data, "yhat_upper"),
         "yhat_lower": convert_ts(data, "yhat_lower"),
         "anomalies": anomalies,
+        "score": convert_ts(data, "score"),
+        "final_score": convert_ts(data, "final_score"),
+        "yhat": convert_ts(data, "yhat"),
     }
     return results
