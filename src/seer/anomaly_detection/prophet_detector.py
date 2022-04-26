bd551ea70e73d8f8f018795ce610635d36e6ca5c
trillville <tillman.elser@sentry.io>
2022-04-26 13:56:22 -0700

    dates are no longer passed in unix timestamps

--- src/seer/anomaly_detection/prophet_detector.py
+++ src/seer/anomaly_detection/prophet_detector.py
@@ -44,7 +44,7 @@ class ProphetDetector(Prophet):
 
         """
         train = data.rename(columns={"time": "ds", "count": "y"})
-        train["ds"] = pd.to_datetime(train["ds"], unit="s")
+        train["ds"] = pd.to_datetime(train["ds"])
 
         # no need to preprocess data if it is constant
         if train["y"].nunique() != 1:
