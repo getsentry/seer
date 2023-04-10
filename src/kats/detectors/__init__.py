0a12ce45de1561c28138159f6926c691b97c7496
Neha Pusarla <npusarla98@gmail.com>
2023-04-10 14:29:00 -0700

    remove init for all other detectors

--- src/kats/detectors/__init__.py
+++ src/kats/detectors/__init__.py
@@ -4,10 +4,4 @@
 # LICENSE file in the root directory of this source tree.
 
 from . import cusum_detection  # noqa # usort: skip
-from . import cusum_model  # noqa # usort: skip
-from . import detector  # noqa # usort: skip
-from . import detector_consts  # noqa # usort: skip
-from . import hourly_ratio_detection  # noqa # usort: skip
 
-from . import residual_translation  # noqa # usort: skip
-from . import robust_stat_detection  # noqa # usort: skip
