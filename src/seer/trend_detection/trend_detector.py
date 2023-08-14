6fcddae5c9f6ee1e9946a761c881a9e950deefb5
Neha Pusarla <npusarla98@gmail.com>
2023-08-14 13:39:36 -0700

    keep backend the same

--- src/seer/trend_detection/trend_detector.py
+++ src/seer/trend_detection/trend_detector.py
@@ -18,7 +18,7 @@ import datetime
 from seer.trend_detection.detectors.cusum_detection import CUSUMDetector
 
 
-def find_trends(txns_data, sort_function, zerofilled, trend_perc, pval=0.01):
+def find_trends(txns_data, sort_function, zerofilled, trend_perc=0.05, pval=0.01):
     trend_percentage_list = []
 
     # defined outside for loop so error won't throw for empty data
