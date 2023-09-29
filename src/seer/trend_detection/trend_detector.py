dd707f37fab6e0d712a33ab5d938c04cdb58dfb6
Tillman Elser <trillville@users.noreply.github.com>
2023-09-28 21:40:20 -0700

    update start/end logic

--- src/seer/trend_detection/trend_detector.py
+++ src/seer/trend_detection/trend_detector.py
@@ -98,12 +98,12 @@ def find_trends(txns_data, sort_function, zerofilled, allow_midpoint, trend_perc
             change_point = int(datetime.datetime.timestamp(change_point))
             change_index = timestamps.index(change_point)
 
-        #check the midpoint boolean - don't get midpoint of the request period if this boolean is false, midpoint should only be used for trends
-        if not allow_midpoint:
-            continue
-
         # if breakpoint is in the very beginning or no breakpoints are detected, use midpoint analysis instead
-        if num_breakpoints == 0 or change_index <= req_start_index + 15 or change_index >= len(timestamps) - 10:
+        if num_breakpoints == 0 or change_index <= 5:
+            #check the midpoint boolean - don't get midpoint of the request period if this boolean is false, midpoint should only be used for trends
+            if not allow_midpoint:
+                continue
+
             change_point = (req_start + req_end) // 2
 
 
