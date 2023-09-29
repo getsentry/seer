4779d3e5ac20209376ad674c8aa3deda7992c0ed
Tillman Elser <trillville@users.noreply.github.com>
2023-09-29 12:35:58 -0700

    revert change added during testing

--- src/seer/trend_detection/trend_detector.py
+++ src/seer/trend_detection/trend_detector.py
@@ -99,7 +99,7 @@ def find_trends(txns_data, sort_function, zerofilled, allow_midpoint, trend_perc
             change_index = timestamps.index(change_point)
 
         # if breakpoint is in the very beginning or no breakpoints are detected, use midpoint analysis instead
-        if num_breakpoints == 0 or change_index <= 5 or change_index >= len(timestamps) - 3:
+        if num_breakpoints == 0 or change_index <= 5:
             #check the midpoint boolean - don't get midpoint of the request period if this boolean is false, midpoint should only be used for trends
             if not allow_midpoint:
                 continue
