b5b46261c61178542d30a305dc4897317a3a3d35
Tillman Elser <trillville@users.noreply.github.com>
2023-10-24 12:24:33 -0700

    update severity inference to use new model (#98)

--- src/seer/severity/severity_inference.py
+++ src/seer/severity/severity_inference.py
@@ -1,41 +1,32 @@
 import sentry_sdk
 import torch
 import numpy as np
-from transformers import RobertaForSequenceClassification, RobertaTokenizerFast
+from transformers import AutoTokenizer, AutoModel
 from joblib import load
 
 
 class SeverityInference:
     def __init__(self, embeddings_path, tokenizer_path, classifier_path):
         """Initialize the inference class with pre-trained models and tokenizer."""
-        self.embeddings_model = RobertaForSequenceClassification.from_pretrained(
+        self.embeddings_model = AutoModel.from_pretrained(
             embeddings_path
         )
-        self.tokenizer = RobertaTokenizerFast.from_pretrained(tokenizer_path)
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
         self.device = (
             torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
         )
         self.classifier = load(classifier_path)
 
-    def get_embeddings(self, text, max_len=128):
+    def get_embeddings(self, text, max_len=512):
         """Generate embeddings for the given text using the pre-trained model."""
         # Tokenize the input string
         inputs = self.tokenizer(
             text, return_tensors="pt", padding=True, truncation=True, max_length=max_len
         )
-        input_ids = inputs["input_ids"].to(self.device)
-        attention_mask = inputs["attention_mask"].to(self.device)
-
-        # Move model to the specified device and set it to eval mode
-        model = self.embeddings_model.to(self.device)
-        model.eval()
-
-        # Forward pass
         with torch.no_grad():
-            outputs = model.roberta(input_ids, attention_mask)
-            embeddings = outputs.last_hidden_state[:, 0, :].squeeze()
-
-        return embeddings.cpu().numpy()
+            outputs = self.embeddings_model(**inputs).last_hidden_state[:, 0, :].squeeze().cpu()
+        
+        return outputs.numpy()
 
     def severity_score(self, data):
         """Predict the severity score for the given text using the pre-trained classifier."""
@@ -46,14 +37,13 @@ class SeverityInference:
             log_level_error = (
                 1 if data.get("log_level", "").lower() in ["error", "fatal"] else 0
             )
-            project_score = 0.052209
             handled = data.get("handled", 0)
 
             input_data = np.append(
                 embeddings.reshape(1, -1),
-                [[has_stacktrace, log_level_error, project_score, handled]],
+                [[has_stacktrace, log_level_error, handled]],
                 axis=1,
             )
 
-            pred = self.classifier.predict(input_data)[0]
+            pred = self.classifier.predict_proba(input_data)[0][1]
         return min(1.0, max(0.0, pred))
