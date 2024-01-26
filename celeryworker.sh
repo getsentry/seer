4961253a81a355fd3ef6338f826d292e9f688aef
jenn.muengtaweepongsa <jenn.muengtaweepongsa@sentry.io>
2024-01-26 12:45:06 -0800

    enable celery worker with env CELERY_WORKER_ENABLE

---
+++ celeryworker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$CELERY_WORKER_ENABLE" = "true" ]; then
+    exec celery -A src.celery_app.tasks worker --loglevel=debug
+else
+    echo "Celery worker is disabled via environment variable CELERY_WORKER_ENABLE"
+    exit 0
+fi
