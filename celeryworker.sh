f37464898057840b069707d9bf0ec387835fa1b1
jenn.muengtaweepongsa <jenn.muengtaweepongsa@sentry.io>
2024-01-29 10:18:04 -0800

    4 workers

--- celeryworker.sh
+++ celeryworker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$CELERY_WORKER_ENABLE" = "true" ]; then
-    exec celery -A src.celery_app.tasks worker --loglevel=debug -c 4 # 4 workers for now because we don't acquire locks on the embedding files
+    exec celery -A src.celery_app.tasks worker --loglevel=info -c 4 # 4 workers for now
 else
     echo "Celery worker is disabled via environment variable CELERY_WORKER_ENABLE"
     exit 0
