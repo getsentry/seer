d2df2c0963fdf8fad1ba52751387cd2a466146eb
jenn.muengtaweepongsa <jenn.muengtaweepongsa@sentry.io>
2024-01-31 15:55:34 -0800

    add slack

--- src/seer/automation/autofix/repo_client.py
+++ src/seer/automation/autofix/repo_client.py
@@ -153,7 +153,7 @@ class RepoClient:
 
             This will greatly help us improve the autofix system. Thank you! 🙏
 
-            If there are any questions, please reach out to the [AI/ML Team](https://github.com/orgs/getsentry/teams/machine-learning-ai)
+            If there are any questions, please reach out to the [AI/ML Team](https://github.com/orgs/getsentry/teams/machine-learning-ai) on [#discuss-ai](https://sentry.slack.com/archives/C04SUT3KUV8)
 
             ### 🤓 Stats for the nerds:
             {stats}"""
