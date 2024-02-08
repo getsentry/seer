353bad7677bcca5401ab90c96b0791160a92c897
Jenn Mueng <30991498+jennmueng@users.noreply.github.com>
2024-02-08 12:39:08 -0800

    Wrap deletion line in try-except to handle UnknownObjectException

--- src/seer/automation/autofix/repo_client.py
+++ src/seer/automation/autofix/repo_client.py
@@ -145,7 +145,10 @@ class RepoClient:
 
         if comparison.ahead_by < 1:
             # Remove the branch if there are no changes
-            self.repo.get_git_ref(branch_ref.ref).delete()
+            try:
+                self.repo.get_git_ref(branch_ref.ref).delete()
+            except UnknownObjectException:
+                logger.warning(f'Branch {branch_ref.ref} has no commits and does not exist. Skipping deletion.')
             sentry_sdk.capture_message(
                 f"Failed to create branch from changes. Comparison is ahead by {comparison.ahead_by}"
             )
