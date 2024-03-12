5536e9cb3c2ab6afe1d218c46531be72861ecf22
jenn.muengtaweepongsa <jenn.muengtaweepongsa@sentry.io>
2024-03-12 11:33:03 -0700

    fix(autofix): Fix ast branch parent finder

---
+++ tests/automation/codebase/test_ast.py
@@ -0,0 +1,83 @@
+from typing import Any, Literal
+
+from pydantic import BaseModel
+
+from seer.automation.codebase.ast import find_first_parent_declaration
+from seer.generator import parameterize
+
+
+class FakeNode(BaseModel):
+    type: Literal[
+        "class_declaration",
+        "method_definition",
+        "function_declaration",
+        "lexical_declaration",
+        "something_else",
+        "class_definition",
+        "method_definition",
+        "function_definition",
+        "code",
+        "blah",
+        "foo",
+    ]
+    parent: Any = None
+    children: list[Any] = []
+
+
+@parameterize
+def test_find_first_parent_declaration_js_declaration(
+    root: FakeNode,
+    parents: list[FakeNode],
+    language: Literal["javascript", "typescript", "jsx", "tsx"],
+):
+    valid_declaration_types = [
+        "class_declaration",
+        "method_definition",
+        "function_declaration",
+        "lexical_declaration",
+    ]
+    child = root
+    first_parent = None
+    for parent in parents:
+        if first_parent is None and parent.type in valid_declaration_types:
+            first_parent = parent
+        root.parent = parent
+        root = parent
+    parent = find_first_parent_declaration(child, language, max_depth=len(parents))  # type: ignore
+    assert parent == first_parent
+
+
+@parameterize
+def test_find_first_parent_declaration_py_declaration(
+    root: FakeNode,
+    parents: list[FakeNode],
+    language: Literal["python"],
+):
+    valid_declaration_types = ["class_definition", "method_definition", "function_definition"]
+    child = root
+    first_parent = None
+    for parent in parents:
+        if first_parent is None and parent.type in valid_declaration_types:
+            first_parent = parent
+        root.parent = parent
+        root = parent
+    parent = find_first_parent_declaration(child, language, max_depth=len(parents))  # type: ignore
+    assert parent == first_parent
+
+
+@parameterize
+def test_max_depth(
+    root: FakeNode,
+    parents: list[FakeNode],
+    language: Literal["python"],
+):
+    valid_declaration_types = ["class_definition", "method_definition", "function_definition"]
+    child = root
+    first_parent = None
+    for i, parent in enumerate(parents):
+        if i == 0 and first_parent is None and parent.type in valid_declaration_types:
+            first_parent = parent
+        root.parent = parent
+        root = parent
+    parent = find_first_parent_declaration(child, language, max_depth=1)  # type: ignore
+    assert parent == first_parent
