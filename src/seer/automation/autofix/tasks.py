271b62619511a8730b188098fa7f44b199b0acf0
jenn.muengtaweepongsa <jenn.muengtaweepongsa@sentry.io>
2024-03-13 09:02:47 -0700

    force new group state

--- src/seer/automation/autofix/tasks.py
+++ src/seer/automation/autofix/tasks.py
@@ -29,8 +29,12 @@ class ContinuationState(LocalMemoryState[AutofixContinuation]):
 
     def reload_state_from_sentry(self) -> bool:
         try:
-            group_state = AutofixGroupState.model_validate(
-                self.rpc_client.call("get_autofix_state", issue_id=self.val.request.issue.id)
+            # group_state = AutofixGroupState.model_validate(
+            #     self.rpc_client.call("get_autofix_state", issue_id=self.val.request.issue.id)
+            # )
+            # TODO: This is only temp, remove this
+            group_state = AutofixGroupState(
+                status=AutofixStatus.PROCESSING, steps=[], fix=None, completedAt=None
             )
             logger.info(f"Loaded group_state: {group_state!r}")
             self.val = self.val.model_copy(update=group_state.model_dump())
