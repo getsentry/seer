0054fa55e9eaa9424b889d1e41b6a36f97a23e07
jenn.muengtaweepongsa <jenn.muengtaweepongsa@sentry.io>
2024-03-13 22:31:15 -0700

    fix(autofix): Fix replacements introducing newlines

--- tests/automation/autofix/test_autofix_tools.py
+++ tests/automation/autofix/test_autofix_tools.py
@@ -55,3 +55,69 @@ class TestReplaceSnippetWith(unittest.TestCase):
                 description="message",
             )
         )
+
+    @patch("seer.automation.autofix.components.snippet_replacement.GptClient")
+    def test_replace_snippet_with_chunk_newlines(self, mock_gpt_client):
+        mock_context = MagicMock()
+        mock_gpt_client = mock_gpt_client
+        code_action_tools = CodeActionTools(context=mock_context)
+
+        # Setup
+        original_snippet = "print('Hello, world!')"
+        replacement_snippet = "print('Goodbye, world!')"
+        file_path = "test_file.py"
+        mock_document = MagicMock()
+        mock_document.text = textwrap.dedent(
+            """\
+
+            def foo():
+                print('Hello, world!')
+                return True
+
+
+            """
+        )
+        mock_codebase = MagicMock()
+        mock_context.get_document_and_codebase.return_value = (mock_codebase, mock_document)
+
+        completion_with_parser = MagicMock()
+        code = textwrap.dedent(
+            """\
+            def foo():
+                print('Goodbye, world!')
+                return True"""
+        )
+        completion_with_parser.return_value = ({"code": code}, MagicMock(), MagicMock())
+        mock_gpt_client.return_value.json_completion = completion_with_parser
+
+        result = code_action_tools.replace_snippet_with(
+            file_path, "repo", original_snippet, replacement_snippet, "message"
+        )
+
+        # Assert
+        self.assertTrue(result)
+        self.assertEquals(result, f"success: Resulting code after replacement:\n```\n{code}\n```\n")
+        mock_codebase.store_file_change.assert_called_once_with(
+            FileChange(
+                change_type="edit",
+                path=file_path,
+                reference_snippet=mock_document.text.strip("\n") + "\n",
+                new_snippet=code + "\n",
+                description="message",
+            )
+        )
+
+        expected_final_document_content = textwrap.dedent(
+            """\
+
+            def foo():
+                print('Goodbye, world!')
+                return True
+
+
+            """
+        )
+        assert (
+            mock_codebase.store_file_change.call_args[0][0].apply(mock_document.text)
+            == expected_final_document_content
+        )
