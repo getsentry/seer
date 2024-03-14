7830a93363c1830b6231cde9ba705028e1eb2220
Jenn Mueng <30991498+jennmueng@users.noreply.github.com>
2024-03-14 11:16:43 -0700

    fix(migrations): again (#421)

---
+++ src/migrations/versions/913d11ce1bea_.py
@@ -0,0 +1,23 @@
+"""empty message
+
+Revision ID: 913d11ce1bea
+Revises: 3b94f1956453, 8f9feb77a3a7
+Create Date: 2024-03-14 18:06:06.533395
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "913d11ce1bea"
+down_revision = ("3b94f1956453", "8f9feb77a3a7")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
