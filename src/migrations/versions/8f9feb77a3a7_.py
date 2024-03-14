40b62d5ef954055e59fb34be110eac77a408bb8c
Tillman Elser <trillville@users.noreply.github.com>
2024-03-14 10:41:36 -0700

    fix(grouping): merge conflict (#420)
    * ref(embeddings-grouping): Modify record table
    
    * add migration
    
    * merge conflict
    
    ---------
    
    Co-authored-by: Jodi Jang <jodi.jang@sentry.io>

---
+++ src/migrations/versions/8f9feb77a3a7_.py
@@ -0,0 +1,23 @@
+"""empty message
+
+Revision ID: 8f9feb77a3a7
+Revises: ad9f58463230
+Create Date: 2024-03-14 17:36:01.746292
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "8f9feb77a3a7"
+down_revision = "ad9f58463230"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
