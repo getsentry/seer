edf2899f865589bb533318c7a841bf5add5e5a56
Ram Senthamarai <ram-senth@users.noreply.github.com>
2024-04-05 11:29:29 -0700

    chore: Use all CUDA devices (#597)

--- celeryworker.sh
+++ celeryworker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$CELERY_WORKER_ENABLE" = "true" ]; then
-    exec celery -A src.celery_app.tasks worker --loglevel=info -c 4 # 4 workers for now
+    exec celery -A src.celery_app.tasks worker --loglevel=info -c 4 # Use 4 processes with one GPU per process
 else
     echo "Celery worker is disabled via environment variable CELERY_WORKER_ENABLE"
     exit 0
