4c852c5ea8b812e8466021c34a38312508603c59
Jenn Mueng <30991498+jennmueng@users.noreply.github.com>
2024-04-22 09:39:35 -0700

    fix(autofix): Bump root cause timeout to 1 hr (#635)
    bump it up to make space for codebase indexing until onboarding calls it separately

--- src/seer/automation/autofix/tasks.py
+++ src/seer/automation/autofix/tasks.py
@@ -48,7 +48,8 @@ def get_autofix_state(group_id: int) -> AutofixContinuation | None:
         return continuation.get()
 
 
-@celery_app.task(time_limit=60 * 30)  # 30 minute task timeout
+# TODO: This timeout probably should be dropped back down once we have onboarding call the separate create codebase task
+@celery_app.task(time_limit=60 * 60)  # 60 minute task timeout
 def run_autofix_root_cause(
     request_data: dict[str, Any],
     autofix_group_state: dict[str, Any] | None = None,
