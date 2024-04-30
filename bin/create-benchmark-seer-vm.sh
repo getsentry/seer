7c396d8ca6e4f25cc2f980aa967bdd50767af4f1
Joshua Ferge <josh.ferge@sentry.io>
2024-04-30 14:28:55 -0700

    command for creating a vm

---
+++ bin/create-benchmark-seer-vm.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+gcloud compute instances create-with-container seer-benchmark-20240430-212403 \
+    --project=ml-ai-420606 \
+    --zone=us-central1-a \
+    --machine-type=c3-highcpu-22 \
+    --network-interface=network-tier=PREMIUM,nic-type=GVNIC,subnet=default \
+    --maintenance-policy=MIGRATE \
+    --provisioning-model=STANDARD \
+    --service-account=996102297610-compute@developer.gserviceaccount.com \
+    --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
+    --tags=http-server \
+    --image=projects/cos-cloud/global/images/cos-stable-109-17800-147-60 \
+    --boot-disk-size=200GB \
+    --boot-disk-type=pd-balanced \
+    --boot-disk-device-name=seer-benchmark \
+    --container-image=us-central1-docker.pkg.dev/ml-ai-420606/seer-benchmar/benchmark@sha256:01836d995f83e62666fb7f6dec24806aca647493c46788aeaa4707097eb383ce \
+    --container-restart-policy=always \
+    --container-privileged \
+    --container-stdin \
+    --container-tty \
+    --container-env=SEVERITY_ENABLED=true,GROUPING_ENABLED=true,PORT=80,SENTRY_BASE_URL=values.sentry_base_url,DATABASE_URL=postgresql\+psycopg://root:seer@10.128.0.7/seer,WEB_CONCURRENCY=11 \
+    --no-shielded-secure-boot \
+    --shielded-vtpm \
+    --shielded-integrity-monitoring \
+    --labels=goog-ec-src=vm_add-gcloud,container-vm=cos-stable-109-17800-147-60
