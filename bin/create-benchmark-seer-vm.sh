71a4c6cd39f37c633acdf04b818f1c7b167308cd
Joshua Ferge <josh.ferge@sentry.io>
2024-04-30 14:28:55 -0700

    command for creating a vm

--- bin/create-benchmark-seer-vm.sh
+++ bin/create-benchmark-seer-vm.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-gcloud compute instances create-with-container seer-benchmark-1 \
+gcloud compute instances create-with-container seer-benchmark-20240430-212403 \
     --project=ml-ai-420606 \
     --zone=us-central1-a \
-    --machine-type=g2-standard-4 \
-    --accelerator=count=1,type=nvidia-l4 \
-    --network-interface=network-tier=PREMIUM,nic-type=GVNIC,subnet=default,private-network-ip=10.128.0.100 \
+    --machine-type=c3-highcpu-22 \
+    --network-interface=network-tier=PREMIUM,nic-type=GVNIC,subnet=default \
+    --maintenance-policy=MIGRATE \
     --provisioning-model=STANDARD \
-    --maintenance-policy=TERMINATE \
     --service-account=996102297610-compute@developer.gserviceaccount.com \
     --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
     --tags=http-server \
