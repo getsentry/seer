34653eff4eefc3cf2065753f761f56f926057d18
Jenn Mueng <30991498+jennmueng@users.noreply.github.com>
2024-05-06 14:44:40 -0700

    feat(celery): Separate gpu/cpu celery queue and workers (#681)
    Separates our celery setup into a cuda and a cpu queue. With 4 processes (1 per gpu) on the cuda worker and 32 processes on the cpu worker.
    
    Initial codebase indexing & Autofix execution will run on the cuda worker.
    
    Next steps is to split out the work even more + move the potential codebase indexing that happens during a re-index to the gpu

--- celeryworker.sh
+++ celeryworker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$CELERY_WORKER_ENABLE" = "true" ]; then
-    exec celery -A src.celery_app.tasks worker --loglevel=info -c 4 # Use 4 processes with one GPU per process
+    exec celery -A src.celery_app.tasks worker --loglevel=info $CELERY_WORKER_OPTIONS
 else
     echo "Celery worker is disabled via environment variable CELERY_WORKER_ENABLE"
     exit 0
