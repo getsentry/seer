fa1bb743aab016720f2c2d32fcca4a6044c6eda1
Jodi Jang <116035587+jangjodi@users.noreply.github.com>
2024-05-08 10:29:31 -0700

    ref(similarity-embeddings): Add hash column, defer stacktrace hash column (#625)
    Add hash column with default value
    Accept hash and return parent_hash in similarity api
    Defer un-used stacktrace_hash column

--- tests/seer/grouping/test_grouping.py
+++ tests/seer/grouping/test_grouping.py
@@ -6,39 +6,9 @@ from seer.inference_models import grouping_lookup
 
 
 class TestGrouping(unittest.TestCase):
-    def test_get_nearest_neighbors_no_hash_has_neighbor(self):
+    def test_get_nearest_neighbors_has_neighbor(self):
         """
-        Tests get_nearest_neighbors when the request has no stacktrace hash and a neighbor exists
-        within the threshold.
-        """
-        with Session() as session:
-            embedding = grouping_lookup().encode_text("stacktrace")
-            grouping_request = GroupingRequest(
-                group_id=1, project_id=1, stacktrace="stacktrace", message="message"
-            )
-            grouping_lookup().insert_new_grouping_record(session, grouping_request, embedding)
-            session.commit()
-
-        grouping_request = GroupingRequest(
-            group_id=2, project_id=1, stacktrace=b"stacktrace", message="message"
-        )
-
-        response = grouping_lookup().get_nearest_neighbors(grouping_request)
-        assert response == SimilarityResponse(
-            responses=[
-                GroupingResponse(
-                    parent_group_id=1,
-                    stacktrace_distance=0.0,
-                    message_distance=0.0,
-                    should_group=True,
-                )
-            ],
-            token=None,
-        )
-
-    def test_get_nearest_neighbors_has_hash_has_neighbor(self):
-        """
-        Tests get_nearest_neighbors when the request has a stacktrace hash and this hash matches
+        Tests get_nearest_neighbors when the request has a hash and and the request's data matches
         an existing record (ie. the neighbor exists within the threshold)
         """
         with Session() as session:
@@ -48,7 +18,7 @@ class TestGrouping(unittest.TestCase):
                 project_id=1,
                 stacktrace="stacktrace",
                 message="message",
-                stacktrace_hash="QYK7aNYNnp5FgSev9Np1soqb1SdtyahD",
+                hash="QYK7aNYNnp5FgSev9Np1soqb1SdtyahD",
             )
             grouping_lookup().insert_new_grouping_record(session, grouping_request, embedding)
             session.commit()
@@ -56,9 +26,9 @@ class TestGrouping(unittest.TestCase):
         grouping_request = GroupingRequest(
             group_id=2,
             project_id=1,
-            stacktrace=b"stacktrace",
+            stacktrace="stacktrace",
             message="message",
-            stacktrace_hash="QYK7aNYNnp5FgSev9Np1soqb1SdtyahD",
+            hash="13501807435378261861369456856144",
             k=1,
             threshold=0.01,
         )
@@ -68,84 +38,80 @@ class TestGrouping(unittest.TestCase):
             responses=[
                 GroupingResponse(
                     parent_group_id=1,
+                    parent_hash="QYK7aNYNnp5FgSev9Np1soqb1SdtyahD",
                     stacktrace_distance=0.0,
                     message_distance=0.0,
                     should_group=True,
                 )
             ],
-            token=None,
         )
 
-    def test_get_nearest_neighbors_no_hash_no_neighbor(self):
+    def test_get_nearest_neighbors_has_neighbor_no_group_id(self):
         """
-        Tests get_nearest_neighbors when the request has no stacktrace hash and no neighbor exists
-        within the threshold. Assert that a new record was added and its id is returned.
-        """
-        grouping_request = GroupingRequest(
-            group_id=2,
-            project_id=1,
-            stacktrace=b"stacktrace",
-            message="message",
-            k=1,
-            threshold=0.01,
-        )
-
-        response = grouping_lookup().get_nearest_neighbors(grouping_request)
-        with Session() as session:
-            new_record = session.query(DbGroupingRecord).filter_by(group_id=2).first()
-
-        assert new_record
-        assert response == SimilarityResponse(responses=[], token=new_record.id)
-
-    def test_get_nearest_neighbors_has_hash_no_neighbor(self):
-        """
-        Tests get_nearest_neighbors when the request has a stacktrace hash and no neighbor exists
-        within the threshold. Assert that a new record was added and its id is returned.
-        """
-        grouping_request = GroupingRequest(
-            group_id=2,
-            project_id=1,
-            stacktrace=b"stacktrace",
-            message="message",
-            stacktrace_hash="QYK7aNYNnp5FgSev9Np1soqb1SdtyahD",
-            k=1,
-            threshold=0.01,
-        )
-
-        response = grouping_lookup().get_nearest_neighbors(grouping_request)
-        with Session() as session:
-            new_record = session.query(DbGroupingRecord).filter_by(group_id=2).first()
-
-        assert new_record
-        assert response == SimilarityResponse(responses=[], token=new_record.id)
-
-    def test_get_nearest_neighbors_no_group_id(self):
-        """
-        Tests get_nearest_neighbors when the request has no group id and the matching record has
-        no group id. Assert that a new record was added and its id is returned.
+        Tests get_nearest_neighbors when the request has only a hash and the request's data matches
+        an existing record (ie. the neighbor exists within the threshold)
         """
         with Session() as session:
             embedding = grouping_lookup().encode_text("stacktrace")
             grouping_request = GroupingRequest(
-                project_id=1, stacktrace="stacktrace", message="message_1"
+                project_id=1,
+                stacktrace="stacktrace",
+                message="message",
+                hash="QYK7aNYNnp5FgSev9Np1soqb1SdtyahD",
             )
             grouping_lookup().insert_new_grouping_record(session, grouping_request, embedding)
             session.commit()
 
         grouping_request = GroupingRequest(
-            project_id=1, stacktrace=b"stacktrace", message="message", k=1, threshold=0.01
+            project_id=1,
+            stacktrace=b"stacktrace",
+            message="message",
+            hash="13501807435378261861369456856144",
+            k=1,
+            threshold=0.01,
+        )
+
+        response = grouping_lookup().get_nearest_neighbors(grouping_request)
+        assert response == SimilarityResponse(
+            responses=[
+                GroupingResponse(
+                    parent_group_id=None,
+                    parent_hash="QYK7aNYNnp5FgSev9Np1soqb1SdtyahD",
+                    stacktrace_distance=0.0,
+                    message_distance=0.0,
+                    should_group=True,
+                )
+            ],
+        )
+
+    def test_get_nearest_neighbors_no_neighbor(self):  # TODO jodi: need to test all no group ids
+        """
+        Test get_nearest_neighbors when no matching record exists. Assert that the record for
+        the group hash was added.
+        """
+        grouping_request = GroupingRequest(
+            hash="QYK7aNYNnp5FgSev9Np1soqb1SdtyahD",
+            project_id=1,
+            stacktrace=b"stacktrace",
+            message="message",
+            k=1,
+            threshold=0.01,
         )
 
         response = grouping_lookup().get_nearest_neighbors(grouping_request)
         with Session() as session:
-            new_record = session.query(DbGroupingRecord).filter_by(message="message").first()
+            new_record = (
+                session.query(DbGroupingRecord)
+                .filter_by(hash="QYK7aNYNnp5FgSev9Np1soqb1SdtyahD")
+                .first()
+            )
 
         assert new_record
-        assert response == SimilarityResponse(responses=[], token=new_record.id)
+        assert response == SimilarityResponse(responses=[])
 
     def test_insert_new_grouping_record_group_record_exists(self):
         """
-        Tests that insert_new_grouping_record only creates one record per group id.
+        Tests that insert_new_grouping_record only creates one record per group hash.
         """
         with Session() as session:
             embedding = grouping_lookup().encode_text("stacktrace")
@@ -154,14 +120,17 @@ class TestGrouping(unittest.TestCase):
                 project_id=1,
                 stacktrace="stacktrace",
                 message="message",
-                stacktrace_hash="QYK7aNYNnp5FgSev9Np1soqb1SdtyahD",
+                hash="QYK7aNYNnp5FgSev9Np1soqb1SdtyahD",
             )
             # Insert the grouping record
-            insert_id = grouping_lookup().insert_new_grouping_record(
-                session, grouping_request, embedding
-            )
+            grouping_lookup().insert_new_grouping_record(session, grouping_request, embedding)
+            session.commit()
             # Re-insert the grouping record
-            existing_id = grouping_lookup().insert_new_grouping_record(
-                session, grouping_request, embedding
+            grouping_lookup().insert_new_grouping_record(session, grouping_request, embedding)
+            session.commit()
+            matching_record = (
+                session.query(DbGroupingRecord)
+                .filter_by(hash="QYK7aNYNnp5FgSev9Np1soqb1SdtyahD")
+                .all()
             )
-            assert insert_id == existing_id
+            assert len(matching_record) == 1
