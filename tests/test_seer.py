202e8ca6705f9cf2c4410d93d0ab05e32b47b48a
Jodi Jang <jodi.jang@sentry.io>
2024-05-10 13:42:01 -0700

    feat(similarity_embedding): Create bulk insert endpoint

--- tests/test_seer.py
+++ tests/test_seer.py
@@ -8,7 +8,7 @@ from johen.pytest import parametrize
 from sqlalchemy import text
 
 from seer.app import app
-from seer.db import AsyncSession, ProcessRequest, Session
+from seer.db import AsyncSession, DbGroupingRecord, ProcessRequest, Session
 from seer.inference_models import dummy_deferred, reset_loading_state, start_loading
 
 
@@ -302,6 +302,69 @@ class TestSeer(unittest.TestCase):
         )
         output = json.loads(response.get_data(as_text=True))
         assert output == {"data": []}
+
+    def test_similarity_grouping_record_endpoint_valid(self):
+        """Test the similarity grouping record endpoint"""
+        record_requests = {
+            "data": [
+                {
+                    "hash": str(i) * 32,
+                    "project_id": 1,
+                    "message": "message " + str(i),
+                }
+                for i in range(5)
+            ],
+            "stacktrace_list": ["stacktrace " + str(i) for i in range(5)],
+        }
+
+        response = app.test_client().post(
+            "/v0/issues/similar-issues/grouping-record",
+            data=json.dumps(record_requests),
+            content_type="application/json",
+        )
+        output = json.loads(response.get_data(as_text=True))
+        assert output == {"success": True}
+        for i in range(5):
+            with Session() as session:
+                assert (
+                    session.query(DbGroupingRecord)
+                    .filter(DbGroupingRecord.hash == str(i) * 32)
+                    .first()
+                    is not None
+                )
+
+    def test_similarity_grouping_record_endpoint_invalid(self):
+        """
+        Test the similarity grouping record endpoint is unsuccessful when input lists are of
+        different lengths
+        """
+        record_requests = {
+            "data": [
+                {
+                    "hash": str(i) * 32,
+                    "project_id": 1,
+                    "message": "message " + str(i),
+                }
+                for i in range(2, 4)
+            ],
+            "stacktrace_list": ["stacktrace " + str(i) for i in range(3)],
+        }
+
+        response = app.test_client().post(
+            "/v0/issues/similar-issues/grouping-record",
+            data=json.dumps(record_requests),
+            content_type="application/json",
+        )
+        output = json.loads(response.get_data(as_text=True))
+        assert output == {"success": False}
+        for i in range(2, 4):
+            with Session() as session:
+                assert (
+                    session.query(DbGroupingRecord)
+                    .filter(DbGroupingRecord.hash == str(i) * 32)
+                    .first()
+                    is None
+                )
 
 
 @parametrize(count=1)
