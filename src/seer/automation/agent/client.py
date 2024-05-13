148cd202ff0ca8440e1531c69245a697e54e7ee4
Tillman Elser <trillville@users.noreply.github.com>
2024-05-13 11:38:25 -0700

    bump openai version (#702)

--- src/seer/automation/agent/client.py
+++ src/seer/automation/agent/client.py
@@ -32,7 +32,7 @@ class LlmClient(ABC):
 
 
 class GptClient(LlmClient):
-    def __init__(self, model: str = "gpt-4-turbo-2024-04-09"):
+    def __init__(self, model: str = "gpt-4o-2024-05-13"):
         self.model = model
         self.openai_client = wrappers.wrap_openai(OpenAI())
 
