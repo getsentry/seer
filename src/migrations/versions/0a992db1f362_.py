ad509d296a35aa89470c3593587e12277ec8adda
Jenn Mueng <30991498+jennmueng@users.noreply.github.com>
2024-05-15 14:09:28 -0700

    merge migration (#717)

---
+++ src/migrations/versions/0a992db1f362_.py
@@ -0,0 +1,23 @@
+"""empty message
+
+Revision ID: 0a992db1f362
+Revises: 704569328ec7, ab24602a9f68
+Create Date: 2024-05-15 21:06:42.859838
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "0a992db1f362"
+down_revision = ("704569328ec7", "ab24602a9f68")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
