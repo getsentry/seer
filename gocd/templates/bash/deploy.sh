b3033e305f5b00cfb5253ce5bbd0bcc5e171396c
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2024-05-16 08:35:24 -0400

    ref: use artifact registry image for deploys (#714)

--- gocd/templates/bash/deploy.sh
+++ gocd/templates/bash/deploy.sh
@@ -5,11 +5,11 @@ eval "$(/devinfra/scripts/regions/project_env_vars.py --region="${SENTRY_REGION}
 /devinfra/scripts/k8s/k8stunnel \
   && /devinfra/scripts/k8s/k8s-deploy.py \
   --label-selector="service=seer" \
-  --image="us.gcr.io/sentryio/seer:${GO_REVISION_SEER_REPO}" \
+  --image="us-central1-docker.pkg.dev/sentryio/seer/image:${GO_REVISION_SEER_REPO}" \
   --container-name="seer"
 
 /devinfra/scripts/k8s/k8stunnel \
   && /devinfra/scripts/k8s/k8s-deploy.py \
   --label-selector="service=seer-autofix" \
-  --image="us.gcr.io/sentryio/seer:${GO_REVISION_SEER_REPO}" \
+  --image="us-central1-docker.pkg.dev/sentryio/seer/image:${GO_REVISION_SEER_REPO}" \
   --container-name="seer-autofix"
