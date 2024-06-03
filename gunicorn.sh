4eeabec22b424ec23fe30ae7b5882a13c6944ec7
jenn.muengtaweepongsa <jenn.muengtaweepongsa@sentry.io>
2024-06-03 09:23:08 -0700

    remove echo from gunicorn.sh

--- gunicorn.sh
+++ gunicorn.sh
@@ -6,6 +6,4 @@ if [ "$DEV" = "true" ] || [ "$DEV" = "1" ]; then
     GUNICORN_ARGS="--reload"
 fi
 
-echo "PORT is $PORT"
-
 exec gunicorn --bind :$PORT --worker-class sync --threads 1 --timeout 0 --access-logfile - src.seer.app:app $GUNICORN_ARGS
