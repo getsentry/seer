3dc54af19ea09e6a833de63474408a128601b00a
Jodi Jang <116035587+jangjodi@users.noreply.github.com>
2024-06-13 14:17:30 -0400

    ref(similarity): Drop group_id column from record table (#794)
    Drop group_id column from grouping record table
    group_id column was previously deferred

--- src/seer/db.py
+++ src/seer/db.py
@@ -237,7 +237,6 @@ class DbPrIdToAutofixRunIdMapping(Base):
 class DbGroupingRecord(Base):
     __tablename__ = "grouping_records"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    group_id: Mapped[Optional[int]] = deferred(mapped_column(BigInteger, nullable=True))
     project_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
     message: Mapped[str] = mapped_column(String, nullable=False)
     stacktrace_embedding: Mapped[Vector] = mapped_column(Vector(768), nullable=False)
