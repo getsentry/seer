f2171d614c62329db4f35a58e1f2dca44da8afee
jenn.muengtaweepongsa <jenn.muengtaweepongsa@sentry.io>
2024-06-25 23:56:09 +0700

    chore(logging): Improve logging for autofix codebase index debugging

--- src/seer/automation/codebase/codebase_index.py
+++ src/seer/automation/codebase/codebase_index.py
@@ -8,7 +8,6 @@ import torch
 import tree_sitter_languages
 from sentence_transformers import SentenceTransformer
 from sentry_sdk.ai.monitoring import ai_track
-from tqdm.auto import tqdm
 from tree_sitter import Tree
 from unidiff import PatchSet
 
@@ -389,15 +388,14 @@ class CodebaseIndex:
         logger.debug(f"Embedding {len(chunks)} chunks...")
         embeddings_list: list[np.ndarray] = []
 
-        with tqdm(total=len(chunks), desc="Embedding chunk") as pbar:
-            for i in range(0, len(chunks), superchunk_size := 256):
-                batch_embeddings: np.ndarray = embedding_model.encode(
-                    [chunk.get_dump_for_embedding() for chunk in chunks[i : i + superchunk_size]],
-                    batch_size=4,  # Batch size of 24 works best on a2-ultragpu-1g instance.
-                    show_progress_bar=False,
-                )
-                embeddings_list.extend(batch_embeddings)
-                pbar.update(superchunk_size)
+        for i in range(0, len(chunks), superchunk_size := 256):
+            batch_embeddings: np.ndarray = embedding_model.encode(
+                [chunk.get_dump_for_embedding() for chunk in chunks[i : i + superchunk_size]],
+                batch_size=4,  # Batch size of 24 works best on a2-ultragpu-1g instance.
+                show_progress_bar=False,
+            )
+            embeddings_list.extend(batch_embeddings)
+
         embeddings = np.array(embeddings_list)
         logger.debug(f"Embedded {len(chunks)} chunks")
 
