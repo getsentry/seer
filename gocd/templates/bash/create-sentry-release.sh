1da7202aa7b400202873d996960ce6ff042fdb19
Zachary Collins <zachary.collins@sentry.io>
2024-07-04 12:20:31 -0700

    Ensure release before deploy

--- gocd/templates/bash/create-sentry-release.sh
+++ gocd/templates/bash/create-sentry-release.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
+sentry-cli releases new "${GO_REVISION_SEER_REPO}"
 sentry-cli releases deploys "${GO_REVISION_SEER_REPO}" new -e production
 sentry-cli releases finalize "${GO_REVISION_SEER_REPO}"
+sentry-cli releases set-commits "${GO_REVISION_SEER_REPO}" --auto || true
