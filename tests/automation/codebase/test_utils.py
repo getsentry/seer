7e5f188c59df84036b3071d863785a88d29f4e22
Jenn Mueng <30991498+jennmueng@users.noreply.github.com>
2024-07-11 03:01:01 +0700

    fix(autofix): Stacktrace frame match relative frame paths (#867)
    Fixes a bug we noticed in our Javascript project with relative stack
    frame paths not being matched.

---
+++ tests/automation/codebase/test_utils.py
@@ -0,0 +1,50 @@
+from seer.automation.codebase.utils import potential_frame_match
+from seer.automation.models import StacktraceFrame
+
+
+class TestPotentialFrameMatch:
+    def test_simple_case(self):
+        frame = StacktraceFrame(
+            filename="src/seer/automation/codebase/utils.py",
+            abs_path="",
+            line_no=1,
+            col_no=1,
+            context=[],
+        )
+
+        assert potential_frame_match("/home/app/src/seer/automation/codebase/utils.py", frame)
+
+    def test_relative_path(self):
+        frame = StacktraceFrame(
+            filename="./src/seer/automation/codebase/utils.py",
+            abs_path="",
+            line_no=1,
+            col_no=1,
+            context=[],
+        )
+
+        assert potential_frame_match("app/src/seer/automation/codebase/utils.py", frame)
+
+    def test_relative_parent_path(self):
+        frame = StacktraceFrame(
+            filename="../src/seer/automation/codebase/utils.py",
+            abs_path="",
+            line_no=1,
+            col_no=1,
+            context=[],
+        )
+
+        assert potential_frame_match("app/src/seer/automation/codebase/utils.py", frame)
+
+    def test_non_matching_path(self):
+        frame = StacktraceFrame(
+            filename="src/seer/automation/codebase/utils.py",
+            abs_path="",
+            line_no=1,
+            col_no=1,
+            context=[],
+        )
+
+        assert not (
+            potential_frame_match("/home/app/src/seer/automation/codebase/test_utils.py", frame)
+        )
