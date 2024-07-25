7c756670c94ce178320ab736e587170aee68d40c
Joshua Li <joshuarli98@gmail.com>
2024-07-25 09:36:12 -0700

    [skip ci] wip

--- devenv/sync.py
+++ devenv/sync.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
-from devenv.lib import config, proc, venv
+import configparser
+
+from devenv.lib import colima, limactl, config, proc, venv
+from devenv import constants
 
 
 def main(context: dict[str, str]) -> int:
@@ -13,6 +16,21 @@ def main(context: dict[str, str]) -> int:
     venv.ensure(venv_dir, python_version, url, sha256)
     venv.sync(reporoot, venv_dir, requirements)
 
-    print("Executing update tasks in Makefile...")
-    proc.run(("make", "-C", reporoot, "update"), exit=True)
+    # install colima
+    repo_config = configparser.ConfigParser()
+    repo_config.read(f"{reporoot}/devenv/config.ini")
+    colima.install(
+        repo_config["colima"]["version"],
+        repo_config["colima"][constants.SYSTEM_MACHINE],
+        repo_config["colima"][f"{constants.SYSTEM_MACHINE}_sha256"],
+        reporoot,
+    )
+    limactl.install(reporoot)
+
+    # start colima if it's not already running
+    colima.start(reporoot)
+
+    # print("Executing update tasks in Makefile...")
+    # proc.run(("make", "-C", reporoot, "update"), exit=True)
+
     return 0
