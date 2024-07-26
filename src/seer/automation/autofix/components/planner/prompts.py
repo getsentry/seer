4b2edc77aab5aa195a1aad0ea930877bd27a9594
Rohan Agarwal <rohan.agarwal@sentry.io>
2024-07-26 10:02:03 -0700

    Tweak prompts to encourage brief thoughts with justifications

--- src/seer/automation/autofix/components/planner/prompts.py
+++ src/seer/automation/autofix/components/planner/prompts.py
@@ -14,7 +14,7 @@ class PlanningPrompts:
 
             You are giving tasks to a coding agent that will perform code changes based on your instructions. The tasks must be clear and detailed enough that the coding agent can perform the task without any additional information.
 
-            You have access to tools that allow you to search a codebase to find the relevant code snippets and view relevant files. You can use these tools as many times as you want to find the relevant code snippets.
+            You have access to tools that allow you to search a codebase to find the relevant code snippets and view relevant files. You can use these tools as many times as you want to find the relevant code snippets. Every time you use a tool, you need to justify why extremely quickly to avoid being fired; to do so, just fill in this one sentence in the <thoughts> tag and say nothing else: Doing X because Y.
 
             Your output must use the below format and use the types of steps provided:
             {steps_example_str}
@@ -23,7 +23,7 @@ class PlanningPrompts:
             - Each code change must be a separate step and be explicit and clear.
             - No placeholders are allowed, the steps must be clear and detailed.
             - Make sure you use the tools provided to look through the codebase and at the files you are changing before outputting the steps.
-            - Use the <thoughts> tag to think step by step before you return your result with a <plan_steps> tag."""
+            - Your result should be in a <plan_steps> tag."""
         ).format(steps_example_str=PlanStepsPromptXml.get_example().to_prompt_str())
 
     @staticmethod
