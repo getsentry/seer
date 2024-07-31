68d0eb835e5a2e282691c74f99fb2aebead6031f
jenn.muengtaweepongsa <jenn.muengtaweepongsa@sentry.io>
2024-07-31 10:50:21 -0700

    feat: Langfuse in DI + disable in tests

---
+++ src/seer/langfuse.py
@@ -0,0 +1,51 @@
+import logging
+
+from langfuse import Langfuse
+from langfuse.decorators import langfuse_context
+
+from seer.configuration import AppConfig
+from seer.dependency_injection import Module, inject, injected
+
+logger = logging.getLogger(__name__)
+
+langfuse_module = Module()
+
+
+@inject
+def initialize_langfuse_context(config: AppConfig = injected):
+    langfuse_context.configure(
+        public_key=config.LANGFUSE_PUBLIC_KEY,
+        secret_key=config.LANGFUSE_SECRET_KEY,
+        host=config.LANGFUSE_HOST,
+        enabled=bool(config.LANGFUSE_HOST),
+    )
+
+
+@langfuse_module.provider
+def provide_langfuse(config: AppConfig = injected) -> Langfuse:
+    return Langfuse(
+        public_key=config.LANGFUSE_PUBLIC_KEY,
+        secret_key=config.LANGFUSE_SECRET_KEY,
+        host=config.LANGFUSE_HOST,
+        enabled=bool(config.LANGFUSE_HOST),
+    )
+
+
+@inject
+def append_langfuse_trace_tags(new_tags: list[str], langfuse: Langfuse = injected):
+    """
+    Appends traces to the current trace in the context.
+    MUST BE RUN WITHIN A LANGFUSE TRACE!
+    """
+    try:
+        trace_id = langfuse_context.get_current_trace_id()
+        if trace_id:
+            trace = langfuse.get_trace(trace_id)
+            langfuse_context.update_current_trace(
+                tags=(trace.tags or []) + new_tags,
+            )
+    except Exception as e:
+        logger.exception(e)
+
+
+langfuse_module.enable()
