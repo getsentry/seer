de3e4c2f3052b2762d0c73cc58f3c935d5c0beeb
jenn.muengtaweepongsa <jenn.muengtaweepongsa@sentry.io>
2024-08-06 10:44:33 -0700

    remove more stuff

--- src/seer/automation/autofix/autofix_context.py
+++ src/seer/automation/autofix/autofix_context.py
@@ -140,9 +140,6 @@ class AutofixContext(PipelineContext):
     def get_file_contents(
         self, path: str, repo_name: str | None = None, ignore_local_changes: bool = False
     ) -> str | None:
-        # @jennmueng: This functionality is duplicated with get_documents in CodebaseIndex,
-        # that one is needed for uses within that class,
-        # we will remove that one if we go with the no-embedding approach
         repo_client = self.get_repo_client(repo_name)
 
         file_contents = repo_client.get_file_content(path)
