830b8e5a96184470282bcb65e11c90751026c66b
Rohan Agarwal <47861399+roaga@users.noreply.github.com>
2024-08-13 18:35:32 -0400

    (autofix): Add breadcrumbs support, pre-filter issue context (#1036)
    Start leveraging breadcrumbs context in Autofix in addition to stack
    traces (part of #998 ).
    
    Add a gpt-4o-mini call before the root cause step and before the coding
    step to filter out context that is probably useless for debugging.
    
    Eval score: 0.46/0.55, which is comparable to current main branch

--- src/seer/automation/autofix/components/root_cause/component.py
+++ src/seer/automation/autofix/components/root_cause/component.py
@@ -4,6 +4,8 @@ from langfuse.decorators import observe
 from sentry_sdk.ai.monitoring import ai_track
 
 from seer.automation.agent.agent import AgentConfig, GptAgent
+from seer.automation.agent.client import GptClient
+from seer.automation.agent.models import Message
 from seer.automation.autofix.autofix_context import AutofixContext
 from seer.automation.autofix.components.root_cause.models import (
     RootCauseAnalysisOutput,
@@ -33,9 +35,20 @@ class RootCauseAnalysisComponent(BaseComponent[RootCauseAnalysisRequest, RootCau
             ),
         )
 
+        # preprocess event information
+        event_details_response, _ = GptClient().completion(
+            messages=[
+                Message(
+                    content=f"We have lots of information available for debugging an issue in our code below. Reproduce the info exactly as it is now, but exclude any useless information.\n\n{request.event_details.format_event()}"
+                ),
+            ],
+            model="gpt-4o-mini-2024-07-18",
+        )
+        simplified_event_details = event_details_response.content
+
         response = agent.run(
             RootCauseAnalysisPrompts.format_default_msg(
-                event=request.event_details,
+                event=simplified_event_details,
                 instruction=request.instruction,
             ),
             context=self.context,
