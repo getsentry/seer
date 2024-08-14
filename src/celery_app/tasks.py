61b4115b9b3968f75a2ef451f4b83dcecdad8025
jenn.muengtaweepongsa <jenn.muengtaweepongsa@sentry.io>
2024-08-14 15:16:38 -0700

    chore: Enable hourly autofix run cleanup task

--- src/celery_app/tasks.py
+++ src/celery_app/tasks.py
@@ -1,19 +1,18 @@
 # All tasks are part of the import dependency tree of starting from seer endpoints.
 # See the test_seer:test_detected_celery_jobs test
+from celery.schedules import crontab
+
 import seer.app  # noqa: F401
 from celery_app.app import celery_app as celery  # noqa: F401
-
-# from celery.schedules import crontab
-# from celery_app.config import CeleryQueues
-# from seer.automation.autofix.tasks import check_and_mark_recent_autofix_runs
+from celery_app.config import CeleryQueues
+from seer.automation.autofix.tasks import check_and_mark_recent_autofix_runs
 
 
 @celery.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
-    # TODO: Enable this task when it's safe, it uses the time values that will be migrated but defaulted to the latest, we wait until we're sure we won't be running on all run_states at once
-    # sender.add_periodic_task(
-    #     crontab(minute=0, hour="*"),
-    #     check_and_mark_recent_autofix_runs.signature(kwargs={}, queue=CeleryQueues.DEFAULT),
-    #     name="Check and mark recent autofix runs every hour",
-    # )
+    sender.add_periodic_task(
+        crontab(minute=0, hour="*"),
+        check_and_mark_recent_autofix_runs.signature(kwargs={}, queue=CeleryQueues.DEFAULT),
+        name="Check and mark recent autofix runs every hour",
+    )
     pass
