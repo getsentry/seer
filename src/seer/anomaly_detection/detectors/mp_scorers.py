fcee886ef154dc8a386192bc712d3fbfc10559c8
Ram Senthamarai <ram.senthamarai@sentry.io>
2024-08-17 06:59:03 -0700

    Not using IQR improves performance

--- src/seer/anomaly_detection/detectors/mp_scorers.py
+++ src/seer/anomaly_detection/detectors/mp_scorers.py
@@ -162,11 +162,12 @@ class MPIRQScorer(MPScorer):
         self, flag: AnomalyFlags, ts_value: float, context: npt.NDArray[np.float64]
     ) -> AnomalyFlags:
         if flag == "anomaly_higher_confidence" or flag == "anomaly_lower_confidence":
-            [Q1, Q3] = np.quantile(context, [0.30, 0.70])
-            # IQR = Q3 - Q1
-            # threshold_lower = Q1 - (1.2 * IQR)
-            # threshold_upper = Q3 + (1.2 * IQR)
-            if ts_value > Q1 and ts_value < Q3:
+            [Q1, Q3] = np.quantile(context, [0.25, 0.75])
+            IQR = Q3 - Q1
+            threshold_lower = Q1 - (0 * IQR)
+            threshold_upper = Q3 + (0 * IQR)
+            # if ts_value > Q1 and ts_value < Q3:
+            if ts_value >= threshold_lower and ts_value <= threshold_upper:
                 flag = "anomaly_lower_confidence"
             else:
                 flag = "anomaly_higher_confidence"
