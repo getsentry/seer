2d1f028f701a5a67c9303df12309794f2e22cb32
Aayush Seth <aayush.seth@sentry.io>
2024-09-03 14:53:53 -0700

    Add basic tests for batch detection

---
+++ tests/seer/anomaly_detection/test_anomaly_detection.py
@@ -0,0 +1,88 @@
+import unittest
+
+from seer.anomaly_detection.accessors import DbAlertDataAccessor
+from seer.anomaly_detection.anomaly_detection import AnomalyDetection
+from seer.anomaly_detection.models.external import (
+    AlertInSeer,
+    AnomalyDetectionConfig,
+    DetectAnomaliesRequest,
+    DetectAnomaliesResponse,
+    StoreDataRequest,
+    StoreDataResponse,
+)
+from tests.seer.anomaly_detection.timeseries.timeseries import context
+
+# from typing import List
+
+
+class TestAnomalyDetection(unittest.TestCase):
+
+    def test_detect_anomalies(self):
+
+        # setup different params for online and batch detection
+
+        config = AnomalyDetectionConfig(
+            time_period=15, sensitivity="low", direction="both", expected_seasonality="auto"
+        )
+
+        # Batch Detection
+        contextBatchTimeseries = context
+
+        requestBatch = DetectAnomaliesRequest(
+            organization_id=1, project_id=1, config=config, context=contextBatchTimeseries
+        )
+
+        responseBatch = AnomalyDetection().detect_anomalies(request=requestBatch)
+
+        # assert that the response is a timeseries (DetectAnomaliesResponse)
+        self.assertIsInstance(responseBatch, DetectAnomaliesResponse)
+
+        # Online Detection
+        # contextOnline = AlertInSeer(id=1)
+
+        # requestOnline = DetectAnomaliesRequest(
+        #     organization_id=1,
+        #     project_id=1,
+        #     config=config,
+        #     context=contextOnline
+        # )
+
+        # responseOnline = AnomalyDetection().detect_anomalies(request=requestOnline)
+
+        # # assert that the response is a timeseries (DetectAnomaliesResponse)
+        # self.assertIsInstance(responseOnline, DetectAnomaliesRequest)
+        # self.assertIsInstance(responseOnline.timeseries, List[TimeSeriesPoint])
+
+    def test_store_data(self):
+
+        # Set up request and accessor
+        organization_id = 1
+        project_id = 1
+        alert = AlertInSeer(id=1)
+        # TODO: Consider looping through config options? Would that be too expensive/brittle?
+        config = AnomalyDetectionConfig(
+            time_period=15, sensitivity="low", direction="both", expected_seasonality="auto"
+        )
+
+        # Load in sample timeseries
+        ts = context
+
+        request = StoreDataRequest(
+            organization_id=organization_id,
+            project_id=project_id,
+            alert=alert,
+            config=config,
+            timeseries=ts,
+        )
+        alert_data_accessor = DbAlertDataAccessor()
+
+        response = AnomalyDetection().store_data(
+            request=request, alert_data_accessor=alert_data_accessor
+        )
+
+        # Successful
+        self.assertEqual(
+            response,
+            StoreDataResponse(success=True),
+            "Store Data Response should be successful",
+        )
