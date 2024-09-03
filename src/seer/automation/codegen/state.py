bfeca3bcea64dac027396fe1222d07b4759fd574
jenn.muengtaweepongsa <jenn.muengtaweepongsa@sentry.io>
2024-09-03 10:12:39 -0700

    unittest gen wip

---
+++ src/seer/automation/codegen/state.py
@@ -0,0 +1,28 @@
+import dataclasses
+from typing import cast
+
+from pydantic import BaseModel
+
+from seer.automation.codegen.models import CodegenContinuation
+from seer.automation.state import DbState
+from seer.db import DbRunState, Session
+
+
+@dataclasses.dataclass
+class CodegenContinuationState(DbState[CodegenContinuation]):
+    @classmethod
+    def from_id(cls, id: int, model: type[BaseModel]) -> "CodegenContinuationState":
+        return cast(CodegenContinuationState, super().from_id(id, model))
+
+    def set(self, state: CodegenContinuation):
+        state.mark_updated()
+
+        with Session() as session:
+            db_state = DbRunState(
+                id=self.id,
+                value=state.model_dump(mode="json"),
+                updated_at=state.updated_at,
+                last_triggered_at=state.last_triggered_at,
+            )
+            session.merge(db_state)
+            session.commit()
---
+++ src/seer/automation/codegen/unittest_step.py
@@ -0,0 +1,69 @@
+from typing import Any
+
+from langfuse.decorators import observe
+from sentry_sdk.ai.monitoring import ai_track
+
+from celery_app.app import celery_app
+from seer.automation.autofix.config import (
+    AUTOFIX_EXECUTION_HARD_TIME_LIMIT_SECS,
+    AUTOFIX_EXECUTION_SOFT_TIME_LIMIT_SECS,
+)
+from seer.automation.codegen.models import CodeUnitTestRequest
+from seer.automation.codegen.step import CodegenStep
+from seer.automation.codegen.unit_test_coding_component import UnitTestCodingComponent
+from seer.automation.models import RepoDefinition
+from seer.automation.pipeline import PipelineStepTaskRequest
+
+
+class UnittestStepRequest(PipelineStepTaskRequest):
+    pr_id: int
+    repo_definition: RepoDefinition
+
+
+@celery_app.task(
+    time_limit=AUTOFIX_EXECUTION_HARD_TIME_LIMIT_SECS,
+    soft_time_limit=AUTOFIX_EXECUTION_SOFT_TIME_LIMIT_SECS,
+)
+def unittest_task(*args, request: dict[str, Any]):
+    UnittestStep(request).invoke()
+
+
+class UnittestStep(CodegenStep):
+    """
+    This class represents the unittest step in the codegen pipeline. It is responsible for
+    generating unit tests based on the provided code changes in a pull request.
+    """
+
+    name = "UnittestStep"
+    max_retries = 2
+
+    @staticmethod
+    def _instantiate_request(request: dict[str, Any]) -> UnittestStepRequest:
+        return UnittestStepRequest.model_validate(request)
+
+    @staticmethod
+    def get_task():
+        return unittest_task
+
+    @observe(name="Codegen - Unittest Step")
+    @ai_track(description="Codegen - Unittest Step")
+    def _invoke(self, **kwargs):
+        self.logger.info("Executing Codegen - Unittest Step")
+        self.context.event_manager.mark_running()
+
+        repo_client = self.context.get_repo_client()
+        pr = repo_client.repo.get_pull(self.request.pr_id)
+
+        diff_content = repo_client.get_pr_diff_content(pr.url)
+
+        unittest_output = UnitTestCodingComponent(self.context).invoke(
+            CodeUnitTestRequest(
+                diff=diff_content,
+            )
+        )
+
+        if unittest_output:
+            for file_change in unittest_output.diffs:
+                self.context.event_manager.append_file_change(file_change)
+
+        self.context.event_manager.mark_completed()
