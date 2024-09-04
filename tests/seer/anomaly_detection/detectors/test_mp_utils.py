b151e77aab4db53435513ee90f96d8a43f353886
Aayush Seth <aayush.seth@sentry.io>
2024-09-04 16:05:21 -0700

    Complete test for mp_utils

--- tests/seer/anomaly_detection/detectors/test_mp_utils.py
+++ tests/seer/anomaly_detection/detectors/test_mp_utils.py
@@ -1,25 +1,74 @@
 import unittest
+from unittest.mock import MagicMock
 
+import numpy as np
+
+from seer.anomaly_detection.detectors import MPConfig, Normalizer
 from seer.anomaly_detection.detectors.mp_utils import MPUtils
 
 
 class TestMPUtils(unittest.TestCase):
 
     def setUp(self):
-        self.scorer = MPUtils()
+        self.utils = MPUtils()
 
-    # TODO: Fix the mocks
-    # @patch(MPConfig)
-    # @patch(normalizer)
-    def test_get_mp_dist_from_mp(self):
-        # mp = []
-        # pad_to_len = []  # TODO: determine if should loop over multiple vals
-        pass
-        # self.scorer.get_mp_dist_from_mp(mp, pad_to_len, pad_to_len, mp_config, normalizer)
+        # Mock MPConfig and Normalizer
+        self.mock_mp_config = MagicMock(spec=MPConfig)
+        self.mock_normalizer = MagicMock(spec=Normalizer)
 
-        # TODO: Test cases
-        # mp_config is none
-        # mp_config is not none and normalize_np is not none
-        # normalizer is none --> exception
-        # pad_to_len is none
-        # all values are fine
+    def test_no_padding_no_normalization(self):
+        mp = np.array([[1.0], [2.0], [3.0], [4.0]])
+        self.mock_mp_config.normalize_mp = False
+
+        result = self.utils.get_mp_dist_from_mp(
+            mp, pad_to_len=None, mp_config=self.mock_mp_config, normalizer=None
+        )
+
+        expected = np.array([1.0, 2.0, 3.0, 4.0], dtype=float)
+        np.testing.assert_array_equal(result, expected)
+
+    def test_no_padding_with_normalization(self):
+        mp = np.array([[1.0], [2.0], [3.0], [4.0]])
+        self.mock_mp_config.normalize_mp = True
+        self.mock_normalizer.normalize.return_value = np.array([0.0, 0.33, 0.67, 1.0])
+
+        result = self.utils.get_mp_dist_from_mp(
+            mp, pad_to_len=None, mp_config=self.mock_mp_config, normalizer=self.mock_normalizer
+        )
+
+        expected = np.array([0.0, 0.33, 0.67, 1.0], dtype=float)
+        np.testing.assert_array_equal(result, expected)
+
+    def test_padding_no_normalization(self):
+        mp = np.array([[1.0], [2.0]])
+        self.mock_mp_config.normalize_mp = False
+
+        result = self.utils.get_mp_dist_from_mp(
+            mp, pad_to_len=5, mp_config=self.mock_mp_config, normalizer=None
+        )
+
+        expected = np.array([np.nan, np.nan, np.nan, 1.0, 2.0], dtype=np.float64)
+        np.testing.assert_array_equal(result, expected)
+
+    def test_padding_with_normalization(self):
+        mp = np.array([[1.0], [2.0]])
+        self.mock_mp_config.normalize_mp = True
+        self.mock_normalizer.normalize.return_value = np.array([0.0, 1.0])
+
+        result = self.utils.get_mp_dist_from_mp(
+            mp, pad_to_len=5, mp_config=self.mock_mp_config, normalizer=self.mock_normalizer
+        )
+
+        expected = np.array([np.nan, np.nan, np.nan, 0.0, 1.0], dtype=np.float64)
+        np.testing.assert_array_equal(result, expected)
+
+    def test_no_normalizer_with_normalization(self):
+        mp = np.array([[1.0], [2.0], [3.0], [4.0]])
+        self.mock_mp_config.normalize_mp = True
+
+        with self.assertRaises(Exception) as context:
+            self.utils.get_mp_dist_from_mp(
+                mp, pad_to_len=None, mp_config=self.mock_mp_config, normalizer=None
+            )
+
+        self.assertTrue("Need normalizer to normalize MP" in str(context.exception))
