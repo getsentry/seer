93c1f1c705724148d1bd6ee64102faec0ff4fe3b
Jenn Mueng <30991498+jennmueng@users.noreply.github.com>
2024-09-04 11:50:01 -0700

    feat(security): Add API bearer token auth via JWT (#1063)
    Mainly for the staging deployment in `ml-ai`
    
    Introduces an `ENFORCE_API_AUTH` env variable that will enforce either
    the rpc secret signing or the bearer token.

--- src/seer/json_api.py
+++ src/seer/json_api.py
@@ -5,21 +5,70 @@ import inspect
 import logging
 from typing import Any, Callable, Type, TypeVar, get_type_hints
 
+import jwt
 import sentry_sdk
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
 from flask import Blueprint, request
+from google.cloud import secretmanager
 from pydantic import BaseModel, ValidationError
-from werkzeug.exceptions import BadRequest, Unauthorized
+from werkzeug.exceptions import BadRequest, InternalServerError, Unauthorized
 
+from seer.bootup import module, stub_module
 from seer.configuration import AppConfig
 from seer.dependency_injection import inject, injected
 
 logger = logging.getLogger(__name__)
 
+
 _F = TypeVar("_F", bound=Callable[..., Any])
 
 
+def access_secret(project_id: str, secret_id: str, version_id: str = "latest"):
+    client = secretmanager.SecretManagerServiceClient()
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+    response = client.access_secret_version(request={"name": name})
+    return response.payload.data.decode("UTF-8")
+
+
+def get_public_key_from_secret(project_id: str, secret_id: str, version_id: str = "latest"):
+    pem_data = access_secret(project_id, secret_id, version_id)
+    public_key = serialization.load_pem_public_key(pem_data.encode(), backend=default_backend())
+    public_key_bytes = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo,
+    )
+
+    return public_key_bytes
+
+
+class PublicKeyBytes(BaseModel):
+    bytes: bytes | None
+
+
+@module.provider
+def provide_public_key(config: AppConfig = injected) -> PublicKeyBytes:
+    return PublicKeyBytes(
+        bytes=(
+            get_public_key_from_secret(
+                config.GOOGLE_CLOUD_PROJECT_ID, config.API_PUBLIC_KEY_SECRET_ID
+            )
+            if config.GOOGLE_CLOUD_PROJECT_ID and config.API_PUBLIC_KEY_SECRET_ID
+            else None
+        )
+    )
+
+
+@stub_module.provider
+def provide_public_key_stub() -> PublicKeyBytes:
+    return PublicKeyBytes(bytes=None)
+
+
 def json_api(blueprint: Blueprint, url_rule: str) -> Callable[[_F], _F]:
-    def decorator(implementation: _F) -> _F:
+    @inject
+    def decorator(
+        implementation: _F, config: AppConfig = injected, public_key: PublicKeyBytes = injected
+    ) -> _F:
         spec = inspect.getfullargspec(implementation)
         annotations = get_type_hints(implementation)
         try:
@@ -43,10 +92,35 @@ def json_api(blueprint: Blueprint, url_rule: str) -> Callable[[_F], _F]:
                 parts = auth_header.split()
                 if len(parts) != 2 or not compare_signature(request.url, raw_data, parts[1]):
                     raise Unauthorized("Rpcsignature did not match for given url and data")
-            else:
-                if config.is_production:
-                    logger.warning(f"Found unexpected authorization header: {auth_header}")
-                    raise Unauthorized("Rpcsignature was not included in authorization header!")
+            elif auth_header.startswith("Bearer "):
+                token = auth_header.split()[1]
+                try:
+                    if public_key.bytes is None:
+                        raise Unauthorized("Public key is not available")
+                    # Verify the JWT token using PyJWT
+                    jwt.decode(token, public_key.bytes, algorithms=["RS256"])
+
+                    # Optionally, you can add additional checks here
+                    # For example, checking the 'exp' claim for token expiration
+                    # or verifying specific claims in the token payload
+
+                    # If the token is successfully decoded and verified,
+                    # the function will continue execution
+                except jwt.ExpiredSignatureError:
+                    raise Unauthorized("Token has expired")
+                except jwt.InvalidSignatureError:
+                    raise Unauthorized("Invalid signature")
+                except jwt.InvalidTokenError:
+                    raise Unauthorized("Invalid token")
+                except Exception as e:
+                    sentry_sdk.capture_exception(e)
+                    print(e)
+                    raise InternalServerError("Something went wrong with the Bearer token auth")
+            elif not config.IGNORE_API_AUTH and config.is_production:
+                logger.warning(f"Found unexpected authorization header: {auth_header}")
+                raise Unauthorized(
+                    "Neither Rpcsignature nor a Bearer token was included in authorization header!"
+                )
 
             # Cached from ^^, this won't result in double read.
             data = request.get_json()
