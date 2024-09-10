a85c7c1cee4e2b8199d3b7cd71a33fcc7aa79779
Aayush Seth <aayush.seth@sentry.io>
2024-09-10 10:17:38 -0700

    Update docstring and variable names

--- tests/seer/anomaly_detection/test_utils.py
+++ tests/seer/anomaly_detection/test_utils.py
@@ -7,13 +7,31 @@ from seer.anomaly_detection.models import TimeSeriesPoint
 
 
 # Returns timeseries and mp_distances as lists of numpy arrays from the synthetic data
-def convert_synthetic_ts(directory: str, as_ts_datatype: bool, include_range: bool = False):
+def convert_synthetic_ts(directory: str, as_ts_datatype: bool, include_anomaly_range: bool = False):
+    """
+    Main entry point for storing time series data for an alert. Synthetic data must be a JSON
+    file with the appropriate key-value pairs (timestamp: int, value: float, mp_dist: float) and
+    with name in the format '<id>_<expected_anomaly>_<window_size>_<anomaly_start>_<anomaly_end>.json'
+
+    Parameters:
+    directory: string
+        Target directory where synthetic data is to be retrieved and parsed from.
+
+    as_ts_datatype: bool
+        Flag for if timeseries should be returned as a List[TimeSeriesPoint] rather than np.array
+
+    include_anomaly_range: bool
+        Flag for if anomaly range [start, end] should be returned.
+
+    Returns:
+    Tuple of lists for each attribute depending on the flag
+    """
 
     timeseries = []
     mp_dists = []
     window_sizes = []
-    window_starts = []
-    window_ends = []
+    anomaly_starts = []
+    anomaly_ends = []
     expected_types = []
 
     # Load in time series JSON files in test_data
@@ -26,6 +44,7 @@ def convert_synthetic_ts(directory: str, as_ts_datatype: bool, include_range: bo
             if filename.split(".")[-1] != "json":
                 raise Exception("File is not a JSON file")
 
+            # Parse file name
             file_params = filename.split(".")[0].split("_")
             expected_type, window_size, start, end = (
                 file_params[1],
@@ -34,7 +53,7 @@ def convert_synthetic_ts(directory: str, as_ts_datatype: bool, include_range: bo
                 int(file_params[4]),
             )
 
-            # Load json and convert to ts and mp_dist
+            # Load json to convert to appropriate outputs and append to respective lists
             with open(f) as file:
 
                 data = json.load(file)
@@ -54,10 +73,10 @@ def convert_synthetic_ts(directory: str, as_ts_datatype: bool, include_range: bo
                 timeseries.append(ts)
                 mp_dists.append(mp_dist)
                 window_sizes.append(window_size)
-                window_starts.append(start)
-                window_ends.append(end)
+                anomaly_starts.append(start)
+                anomaly_ends.append(end)
                 expected_types.append(expected_type)
 
-    if include_range:
-        return expected_types, timeseries, mp_dists, window_sizes, window_starts, window_ends
+    if include_anomaly_range:
+        return expected_types, timeseries, mp_dists, window_sizes, anomaly_starts, anomaly_ends
     return timeseries, mp_dists, window_sizes
