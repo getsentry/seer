6f3c3163be39260385b99f6f36df595fcd2ac873
Rohit <rohit.vinnakota@sentry.io>
2024-09-24 11:07:50 -0400

    Update

--- src/integrations/codecov/codecov_client.py
+++ src/integrations/codecov/codecov_client.py
@@ -7,7 +7,7 @@ class CodecovClient:
     @staticmethod
     @inject
     def fetch_coverage(owner_username, repo_name, pullid, config: AppConfig = injected):
-        token = config.CODECOV_API_TOKEN
+        token = config.CODECOV_SUPER_TOKEN
         url = f"https://api.codecov.io/api/v2/github/{owner_username}/repos/{repo_name}/pulls/{pullid}"
         headers = {"Authorization": f"Bearer {token}", "Accept": "application/json"}
         response = requests.get(url, headers=headers)
@@ -21,7 +21,7 @@ class CodecovClient:
     def fetch_test_results_for_commit(
         owner_username, repo_name, latest_commit_sha, config: AppConfig = injected
     ):
-        token = config.CODECOV_API_TOKEN
+        token = config.CODECOV_SUPER_TOKEN
         url = f"https://api.codecov.io/api/v2/github/{owner_username}/repos/{repo_name}/test-results?commit_id={latest_commit_sha}&outcome=failure"
         headers = {
             "Authorization": f"Bearer {token}",
