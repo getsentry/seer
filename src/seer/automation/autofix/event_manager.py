6fa4c0ab9d7fd9447ccd9308241da269ee1ad6f6
Rohan Agarwal <rohan.agarwal@sentry.io>
2024-10-01 14:55:06 -0700

    Ignore type issue

--- src/seer/automation/autofix/event_manager.py
+++ src/seer/automation/autofix/event_manager.py
@@ -66,7 +66,7 @@ class AutofixEventManager:
             cur_step = cur.find_or_add(step)
             cur_step.status = AutofixStatus.PROCESSING
             cur_step.progress = []
-            cur_step.completedMessage = None
+            cur_step.completedMessage = None  # type: ignore[assignment]
             cur.status = AutofixStatus.PROCESSING
 
     def send_root_cause_analysis_will_start(self):
