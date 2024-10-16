f66d58a74cd78d98c4a9ef09987104ae5378b05b
Rohan Agarwal <47861399+roaga@users.noreply.github.com>
2024-10-16 15:03:13 -0700

    Update Celery tests to remove buggy_code references

--- tests/test_celery.py
+++ tests/test_celery.py
@@ -22,9 +22,9 @@ def test_detected_celery_jobs():
                 "seer.automation.codegen.unittest_step.unittest_task",
                 "seer.automation.tasks.delete_data_for_ttl",
                 "seer.smoke_test.smoke_test",
-                "seer.automation.tasks.buggy_code",  # TODO remove this once testing in prod is done
             ]
         )
+
 
         assert set(k for k in celery_app.conf.beat_schedule.keys()) == set(
             [
