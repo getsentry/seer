dd5b3b55897dfd999f71207c03078c9c3a008b54
jenn.muengtaweepongsa <jenn.muengtaweepongsa@sentry.io>
2024-11-22 14:53:31 -0800

    ideally working?

--- src/seer/automation/codegen/codegen_context.py
+++ src/seer/automation/codegen/codegen_context.py
@@ -2,7 +2,6 @@ import logging
 
 from seer.automation.codebase.repo_client import RepoClient, RepoClientType
 from seer.automation.codegen.codegen_event_manager import CodegenEventManager
-from seer.automation.codegen.models import CodegenContinuation
 from seer.automation.codegen.state import CodegenContinuationState
 from seer.automation.models import RepoDefinition
 from seer.automation.pipeline import PipelineContext
@@ -34,7 +33,7 @@ class CodegenContext(PipelineContext):
 
     @classmethod
     def from_run_id(cls, run_id: int):
-        state = CodegenContinuationState.from_id(run_id, model=CodegenContinuation)
+        state = CodegenContinuationState(run_id)
         with state.update() as cur:
             cur.mark_triggered()
 
