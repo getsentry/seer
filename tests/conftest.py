792bfa87901a25974324e43d0f092f88e9122540
jenn.muengtaweepongsa <jenn.muengtaweepongsa@sentry.io>
2024-12-04 11:36:56 -0800

    try with fake credentials

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,6 +24,7 @@ logger = logging.getLogger(__name__)
 @pytest.fixture(autouse=True, scope="session")
 def configure_environment():
     os.environ["LANGFUSE_HOST"] = ""  # disable Langfuse logging for tests
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/app/tests/test_gcloud_credentials.json"
 
 
 @pytest.fixture
