9a37b8f6993fb26dd7725ac38317cc0374e06ae0
jenn.muengtaweepongsa <jenn.muengtaweepongsa@sentry.io>
2024-12-09 11:41:13 -0800

    fix(autofix): Bump updated_at BEFORE throwing the state in the dbmodel

--- src/seer/automation/autofix/state.py
+++ src/seer/automation/autofix/state.py
@@ -11,7 +11,9 @@ class ContinuationState(DbState[AutofixContinuation]):
     model: type[AutofixContinuation] = AutofixContinuation
     type: DbStateRunTypes = dataclasses.field(default=DbStateRunTypes.AUTOFIX)
 
-    def apply_to_run_state(self, state: AutofixContinuation, run_state: DbRunState):
+    def before_update(self, state: AutofixContinuation):
         state.mark_updated()
+
+    def apply_to_run_state(self, state: AutofixContinuation, run_state: DbRunState):
         run_state.updated_at = state.updated_at
         run_state.last_triggered_at = state.last_triggered_at
