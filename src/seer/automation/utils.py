5a60058593cab620073ac2c04932456902d1814b
Jenn Mueng <30991498+jennmueng@users.noreply.github.com>
2024-12-10 14:33:21 -0800

    ref(autofix): Agent refactor attempt 2 (#1569)
    Tested thoroughly via local e2e getsentry setup
    
    1. Root cause step
    2. Fix step
    3. Fix step after waiting 10 minutes
    4. Rethinking
    5. Interjections
    6. Multiple interjections
    7. Spamming rethinks and interjections
    8. fix feedback
    9. Unit test generation
    10. code diff editing
    11. codecov pr unit test gen
    12. pr creation #1587 
    
    Rethink after 10 minutes to be fixed in a separate pr:
    https://github.com/orgs/getsentry/projects/171/views/7?pane=issue&itemId=90308927
    
    ---------
    
    Co-authored-by: Rohan Agarwal <47861399+roaga@users.noreply.github.com>

--- src/seer/automation/utils.py
+++ src/seer/automation/utils.py
@@ -11,7 +11,9 @@ import torch
 from openai.types.chat import ParsedChatCompletion
 from sentence_transformers import SentenceTransformer
 
-from seer.rpc import DummyRpcClient, RpcClient, SentryRpcClient
+from seer.configuration import AppConfig
+from seer.dependency_injection import inject, injected
+from seer.rpc import RpcClient
 from seer.stubs import DummySentenceTransformer, can_use_model_stubs
 
 # ALERT: Using magic number four. This is temporary code that ensures that AutopFix uses all 4
@@ -107,12 +109,15 @@ def process_repo_provider(provider: str) -> str:
     return provider
 
 
-def check_genai_consent(org_id: int) -> bool:
-    if os.environ.get("NO_SENTRY_INTEGRATION") == "1":
+@inject
+def check_genai_consent(
+    org_id: int, client: RpcClient = injected, config: AppConfig = injected
+) -> bool:
+    if config.NO_SENTRY_INTEGRATION:
         # If we are running in a local environment, we just pass this check
         return True
 
-    response = get_sentry_client().call("get_organization_autofix_consent", org_id=org_id)
+    response = client.call("get_organization_autofix_consent", org_id=org_id)
 
     if response and response.get("consent", False) is True:
         return True
@@ -122,15 +127,6 @@ def check_genai_consent(org_id: int) -> bool:
 def raise_if_no_genai_consent(org_id: int) -> None:
     if not check_genai_consent(org_id):
         raise ConsentError(f"Organization {org_id} has not consented to use GenAI")
-
-
-def get_sentry_client() -> RpcClient:
-    if os.environ.get("NO_SENTRY_INTEGRATION") == "1":
-        rpc_client: DummyRpcClient = DummyRpcClient()
-        rpc_client.dry_run = True
-        return rpc_client
-    else:
-        return SentryRpcClient()
 
 
 def escape_xml_chars(s: str) -> str:
