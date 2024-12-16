84dc584dc8bc4779c3a866f125e6b859e3e5ed90
Aayush Seth <aayush.seth@sentry.io>
2024-12-16 11:24:49 -0800

    update noise parameter calculation

--- src/seer/anomaly_detection/detectors/noise_reducers.py
+++ src/seer/anomaly_detection/detectors/noise_reducers.py
@@ -2,6 +2,7 @@ import abc
 
 import numpy as np
 import numpy.typing as npt
+from numpy.lib.stride_tricks import as_strided
 from pydantic import BaseModel
 
 
@@ -12,14 +13,24 @@ class NoiseReducer(BaseModel, abc.ABC):
 
     @abc.abstractmethod
     def get_noise_parameter(
-        self, timeseries: npt.NDArray, window: int = 12, scale_factor: float = 1.0
+        self, timeseries: npt.NDArray, window: int = 0, scale_factor: float = 1.0
     ) -> float:
         return NotImplemented
 
 
 class VarianceNoiseReducer(NoiseReducer):
+
+    def _get_subsequences(self, arr: npt.NDArray, m: int) -> npt.NDArray:
+        """
+        Gets the vectorized subsequences of size m of the input array
+        """
+        n = arr.size - m + 1
+        s = arr.itemsize
+        subs = as_strided(arr, shape=(n, m), strides=(s, s))
+        return subs
+
     def get_noise_parameter(
-        self, timeseries: npt.NDArray, window: int = 12, scale_factor: float = 1.0
+        self, timeseries: npt.NDArray, window: int = 0, scale_factor: float = 1.0
     ) -> float:
         """
         Gets the noise parameter by calculating the median variance across sliding non-overlapping windows of the timeseries.
@@ -29,9 +40,9 @@ class VarianceNoiseReducer(NoiseReducer):
         timeseries : npt.NDArray
             Input time series array
         window : int, default=12
-            Size of sliding window
+            Size of sliding window for calculating variances. Default is
         scale_factor : float, default=1.0
-            Factor to scale the final noise parameter
+            Factor to scale the final noise parameter. A higher value will result in more aggressive noise reduction.
 
         Returns:
         --------
@@ -39,16 +50,18 @@ class VarianceNoiseReducer(NoiseReducer):
             Noise parameter calculated as median variance * scale_factor
         """
 
-        # TODO: The window should be ~half a day so should be based on ad_config
-        if len(timeseries) == 0 or window <= 0:
-            return 0.0
+        # Limiting our selection to the last num_days
+        # TODO: This should be based on ad_config
+        num_days = 7
+        num_points = 24 * num_days
+        timeseries = timeseries[-num_points:]
 
-        window = min(window, len(timeseries))
+        # Paper suggests 10% of the reference data
+        if window == 0:
+            window = int(0.1 * len(timeseries))
 
-        n_windows = len(timeseries) // window
-        windowed_ts = timeseries[: n_windows * window].reshape(n_windows, window)
-        variances = np.var(windowed_ts, axis=1)
+        variances = np.var(self._get_subsequences(timeseries, window), axis=1)
 
-        noise_parameter = np.median(variances) * scale_factor
-
-        return noise_parameter
+        # Taking the 20th percentile as opposed to the 5th percentile
+        noise_parameter = np.percentile(variances, 20)
+        return noise_parameter * scale_factor
