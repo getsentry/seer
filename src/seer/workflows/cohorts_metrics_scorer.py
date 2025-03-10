1d57e327f0d52c79212a085677da8d6a8f75e1fd
Mitan <dmitrykharkovsky@gmail.com>
2025-03-10 17:05:59 -0400

    Add metric scorer

---
+++ src/seer/workflows/cohorts_metrics_scorer.py
@@ -0,0 +1,57 @@
+from dataclasses import dataclass
+
+import pandas as pd
+from scipy.special import rel_entr
+from scipy.stats import entropy
+
+from seer.workflows.models import MetricWeights
+
+
+@dataclass
+class CohortsMetricsScorer:
+    K_RRF = 60
+
+    @staticmethod
+    def kl_metric_lambda(baseline, selection):
+        return rel_entr(pd.Series(baseline), pd.Series(selection))
+
+    def compute_metrics(self, dataset: pd.DataFrame, metric_weights: MetricWeights) -> pd.DataFrame:
+        dataset = (
+            dataset.pipe(self.compute_kl_score)
+            .pipe(self.compute_entropy_score)
+            .pipe(self.compute_rrf_score, metric_weights)
+        )
+        print(dataset)
+        return dataset
+
+    def compute_kl_score(self, dataset: pd.DataFrame) -> pd.DataFrame:
+        dataset["kl_individual_scores"] = dataset.apply(
+            lambda row: self.kl_metric_lambda(
+                row["distribution_baseline"], row["distribution_selection"]
+            ).to_dict(),
+            axis=1,
+        )
+        dataset["kl_score"] = dataset["kl_individual_scores"].apply(lambda x: sum(x.values()))
+        return dataset
+
+    def compute_entropy_score(self, dataset: pd.DataFrame) -> pd.DataFrame:
+        dataset["entropy_score"] = dataset["distribution_selection"].apply(
+            lambda x: entropy(pd.Series(x))
+        )
+        return dataset
+
+    def compute_rrf_score(
+        self, dataset: pd.DataFrame, metric_weights: MetricWeights
+    ) -> pd.DataFrame:
+        dataset["KL_rank"] = 1 / (
+            self.K_RRF + dataset["kl_score"].rank(method="min", ascending=False)
+        )
+        dataset["entropy_rank"] = 1 / (
+            self.K_RRF + dataset["entropy_score"].rank(method="min", ascending=True)
+        )
+        dataset["RRF_score"] = (
+            metric_weights.kl_divergence_weight * dataset["KL_rank"]
+            + metric_weights.entropy_weight * dataset["entropy_rank"]
+        )
+        dataset.drop(columns=["KL_rank", "entropy_rank"], inplace=True)
+        return dataset
--- src/seer/workflows/compare.py
+++ src/seer/workflows/compare.py
@@ -1,11 +1,12 @@
 from seer.workflows.cohorts_data_processor import DataProcessor
+from seer.workflows.cohorts_metrics_scorer import CohortsMetricsScorer
 from seer.workflows.models import CompareCohortsRequest, CompareCohortsResponse
 
 
 def compare_cohorts(data: CompareCohortsRequest) -> CompareCohortsResponse:
-    data_processor = DataProcessor()
-    dataset = data_processor.prepare_data(data)
-    print(dataset)
+    dataset = DataProcessor().prepare_data(data)
+    scored_dataset = CohortsMetricsScorer().compute_metrics(dataset, data.options.metric_weights)
+    print(scored_dataset)
     mock_results = {
         "results": [
             {"attributeName": "browser", "attributeValues": ["unknown", "safari", "edge"]},
