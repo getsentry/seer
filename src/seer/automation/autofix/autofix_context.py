e0bfc70726d45754fd96a9c771f3a9e7f90c94ba
Rohan Agarwal <rohan.agarwal@sentry.io>
2025-03-12 08:47:21 -0700

    Stronger autocorrect

--- src/seer/automation/autofix/autofix_context.py
+++ src/seer/automation/autofix/autofix_context.py
@@ -131,6 +131,8 @@ class AutofixContext(PipelineContext):
 
     def autocorrect_repo_name(self, repo_name: str) -> str | None:
         repo_names = [repo.full_name for repo in self.repos]
+        if len(repo_names) == 1:
+            return repo_names[0]
         if repo_name and repo_name not in repo_names:
             # Try to find a repo name that contains the provided one to autocorrect
             matching_repos = [r for r in repo_names if repo_name.lower() in r.lower()]
