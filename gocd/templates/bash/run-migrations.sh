54236fb059353f811402d3cc623143bc15de57ac
Tillman Elser <trillville@users.noreply.github.com>
2025-03-13 21:52:05 -0700

    fix migration runner logic

--- gocd/templates/bash/run-migrations.sh
+++ gocd/templates/bash/run-migrations.sh
@@ -5,8 +5,8 @@ if [ "${SENTRY_REGION}" != "customer-1" ] && [ "${SENTRY_REGION}" != "customer-2
     && eval "$(/devinfra/scripts/regions/project_env_vars.py --region="${SENTRY_REGION}")" \
     && /devinfra/scripts/k8s/k8stunnel \
     && /devinfra/scripts/k8s/k8s-spawn-job.py \
-    --container-name="seer-gpu" \
-    --label-selector="service=seer-gpu" \
+    --container-name="seer" \
+    --label-selector="service=seer" \
     -e="IS_DB_MIGRATION=true" \
     "seer-run-migrations" \
     "us-central1-docker.pkg.dev/sentryio/seer/image:${GO_REVISION_SEER_REPO}" \
@@ -18,8 +18,8 @@ else
     && eval "$(/devinfra/scripts/regions/project_env_vars.py --region="${SENTRY_REGION}")" \
     && /devinfra/scripts/k8s/k8stunnel \
     && /devinfra/scripts/k8s/k8s-spawn-job.py \
-    --container-name="seer" \
-    --label-selector="service=seer" \
+    --container-name="seer-gpu" \
+    --label-selector="service=seer-gpu" \
     -e="IS_DB_MIGRATION=true" \
     "seer-run-migrations" \
     "us-central1-docker.pkg.dev/sentryio/seer/image:${GO_REVISION_SEER_REPO}" \
