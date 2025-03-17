a593423667cd2802b8e3941df1608b162db34992
Jenn Mueng <30991498+jennmueng@users.noreply.github.com>
2025-03-17 15:42:14 -0700

    fix(autofix): Fix PR paginated list schenanigans (#2190)
    pulls because it's a `PaginatedList` it evaluates to true when empty....

--- src/seer/automation/codebase/repo_client.py
+++ src/seer/automation/codebase/repo_client.py
@@ -682,7 +682,9 @@ class RepoClient:
         description: str,
         provided_base: str | None = None,
     ) -> PullRequest:
-        if pulls := self.repo.get_pulls(state="open", head=f"{self.repo_owner}:{branch.ref}"):
+        pulls = self.repo.get_pulls(state="open", head=f"{self.repo_owner}:{branch.ref}")
+
+        if pulls.totalCount > 0:
             logger.error(
                 f"Branch {branch.ref} already has an open PR.",
                 extra={
