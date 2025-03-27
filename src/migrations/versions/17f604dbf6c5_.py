79c5e709c1a2d4f6b57525511668d6e5e650df0a
Jenn Mueng <jenn.muengtaweepongsa@sentry.io>
2025-03-27 10:12:12 -0700

    fix migrations

---
+++ src/migrations/versions/17f604dbf6c5_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 17f604dbf6c5
+Revises: a3dade0c6138, c534539c3752
+Create Date: 2025-03-27 17:11:24.070058
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '17f604dbf6c5'
+down_revision = ('a3dade0c6138', 'c534539c3752')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
