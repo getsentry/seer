ca1e0618faef8d81ce4bb128cdd3f21e293dae5e
Jenn Mueng <jenn.muengtaweepongsa@sentry.io>
2025-04-23 18:48:17 +0700

    small refactor

--- src/seer/automation/autofix/tools/tools.py
+++ src/seer/automation/autofix/tools/tools.py
@@ -582,7 +582,7 @@ class BaseTools:
 
     def _get_repo_name_and_path(
         self, kwargs: dict[str, Any], allow_nonexistent_paths: bool = False
-    ) -> tuple[str | None, str | None, str | None]:
+    ) -> tuple[str | None, str | None, str]:
         repos = self._get_repo_names()
 
         path_args = kwargs.get("path", None)
@@ -591,6 +591,7 @@ class BaseTools:
         if not repos:
             return "Error: No repositories found.", None, None
 
+        path: str
         if len(repos) > 1:
             if ":" not in path_args:
                 return (
@@ -607,14 +608,17 @@ class BaseTools:
             path = path_args
 
         fixed_path = self._attempt_fix_path(path, repo_name)
-        if not fixed_path and not allow_nonexistent_paths:
+        if not fixed_path:
+            if allow_nonexistent_paths:
+                return None, repo_name, path
+
             return (
                 f"Error: The path you provided '{path}' does not exist in the repository '{repo_name}'.",
                 None,
                 None,
             )
 
-        return None, repo_name, path
+        return None, repo_name, fixed_path
 
     @observe(name="Claude Tools")
     @sentry_sdk.trace
