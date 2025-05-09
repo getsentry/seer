fc6ee29b1300b7459eee0c031a6a7551ebdbe910
Rohan Agarwal <rohan.agarwal@sentry.io>
2025-05-08 19:41:59 -0700

    Fix

--- src/seer/automation/autofix/components/insight_sharing/component.py
+++ src/seer/automation/autofix/components/insight_sharing/component.py
@@ -160,7 +160,7 @@ def process_sources(sources: list, context: AutofixContext, trace_tree: TraceTre
                 continue
             repo_client = context.get_repo_client(repo_name)
             file_name = source.file_name
-            file_name = context.attempt_fix_path(path=file_name, repo_name=repo_name)
+            file_name = context.autocorrect_file_path(path=file_name, repo_name=repo_name)
             if not file_name:
                 continue
             snippet_to_find = source.code_snippet
