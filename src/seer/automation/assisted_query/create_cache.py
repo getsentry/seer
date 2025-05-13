1ccfd2b312a130cc89485ecfaa1b0eadad5e99da
Aayush Seth <aayush.seth@sentry.io>
2025-05-13 12:59:05 -0700

    merge conflict

--- src/seer/automation/assisted_query/create_cache.py
+++ src/seer/automation/assisted_query/create_cache.py
@@ -48,39 +48,45 @@ def create_cache(data: CreateCacheRequest, client: RpcClient = injected) -> Crea
 
     all_fields = fields_response.get("fields", []) if fields_response else []
 
-    # Filter out numeric tags
-    string_fields = [
-        field for field in all_fields if not (field.startswith("tags[") and ",number]" in field)
-    ]
-    filtered_fields = string_fields
+    filtered_field_values = None
+    if not no_values:
+        # Filter out numeric tags
+        string_fields = [
+            field for field in all_fields if not (field.startswith("tags[") and ",number]" in field)
+        ]
+        filtered_fields = string_fields
 
-    # Include the most important fields for this portion of the query until we reach the limit
-    if len(string_fields) > REQUEST_VALUES_LIMIT:
-        filtered_fields = []
-        for prefix in REQUIRED_FIELD_PREFIXES:
-            filtered_fields.extend([field for field in string_fields if field.startswith(prefix)])
-        for field in string_fields:
-            if len(filtered_fields) >= REQUEST_VALUES_LIMIT:
-                break
-            if field not in filtered_fields:
-                filtered_fields.append(field)
+        # Include the most important fields for this portion of the query until we reach the limit
+        if len(string_fields) > REQUEST_VALUES_LIMIT:
+            filtered_fields = []
+            for prefix in REQUIRED_FIELD_PREFIXES:
+                filtered_fields.extend(
+                    [field for field in string_fields if field.startswith(prefix)]
+                )
+            for field in string_fields:
+                if len(filtered_fields) >= REQUEST_VALUES_LIMIT:
+                    break
+                if field not in filtered_fields:
+                    filtered_fields.append(field)
 
-    filtered_field_values_response = client.call(
-        "get_attribute_values",
-        fields=filtered_fields,
-        org_id=org_id,
-        project_ids=project_ids,
-        stats_period="48h",
-        limit=15,
+        filtered_field_values_response = client.call(
+            "get_attribute_values",
+            fields=filtered_fields,
+            org_id=org_id,
+            project_ids=project_ids,
+            stats_period="48h",
+            limit=15,
+        )
+
+        filtered_field_values = (
+            filtered_field_values_response.get("field_values", {})
+            if filtered_field_values_response
+            else {}
+        )
+
+    cache_prompt = get_cache_prompt(
+        fields=all_fields, field_values=filtered_field_values, no_values=no_values
     )
-
-    filtered_field_values = (
-        filtered_field_values_response.get("field_values", {})
-        if filtered_field_values_response
-        else {}
-    )
-
-    cache_prompt = get_cache_prompt(fields=all_fields, field_values=filtered_field_values)
 
     cache_name = LlmClient().create_cache(
         display_name=cache_diplay_name, contents=cache_prompt, model=get_model_provider()
