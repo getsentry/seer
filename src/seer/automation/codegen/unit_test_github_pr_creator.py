0b04dd4af119d94f4236c61f102b1e6451820e02
Mihir Mavalankar <mihirmavalankar@KGLQ0YTW1Y.local>
2025-05-20 12:30:36 -0700

    set default to True

--- src/seer/automation/codegen/unit_test_github_pr_creator.py
+++ src/seer/automation/codegen/unit_test_github_pr_creator.py
@@ -36,6 +36,7 @@ class GeneratedTestsPullRequestCreator:
             pr_title=pr_title,
             file_changes=self.file_changes_payload,
             branch_name=branch_name,
+            from_base_sha=True,
         )
 
         if not branch_ref:
