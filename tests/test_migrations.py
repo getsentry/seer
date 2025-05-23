8352f380e34808684e79574ad022452ae2d71a41
Mihir Mavalankar <mihirmavalankar@KGLQ0YTW1Y.local>
2025-05-22 17:09:25 -0700

    updated test

--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -13,10 +13,8 @@ def test_run_state_migration(alembic_runner: MigrationContext):
             (2, {}),  # Valid, missing keys
             (3, {"updated_at": None}),  # Valid, key is null
             (4, {"last_triggered_at": None}),  # Valid, key is null
-            (5, {"created_at": None}),  # Valid, key is null
-            (6, {"updated_at": "2020-06-15T13:45:30"}),  # Valid, stuff
-            (7, {"last_triggered_at": "2019-04-12T11:15:31"}),  # Valid, stuff
-            (8, {"created_at": "2019-04-12T11:15:31"}),  # Valid, stuff
+            (5, {"updated_at": "2020-06-15T13:45:30"}),  # Valid, stuff
+            (6, {"last_triggered_at": "2019-04-12T11:15:31"}),  # Valid, stuff
         ):
             session.execute(
                 insert(alembic_runner.table_at_revision("run_state")).values(id=id, value=json)
@@ -26,13 +24,12 @@ def test_run_state_migration(alembic_runner: MigrationContext):
     alembic_runner.migrate_up_to("head")
 
     with Session() as session:
-        for i in range(1, 9):
+        for i in range(1, 7):
             state = session.query(DbRunState).get(i)
             print(state)
             print(
                 f"Row {i}: created_at={state.created_at}, last_triggered_at={state.last_triggered_at}, updated_at={state.updated_at}"
             )
-            # assert state.created_at
+            assert state.created_at
             assert state.last_triggered_at
             assert state.updated_at
-    assert None
