7737f40d455978b97c42e9b6e487bdbf8e506010
Jenn Mueng <jenn.muengtaweepongsa@sentry.io>
2025-05-29 03:17:33 +0700

    fix

--- src/migrations/versions/ac95ebfa16cb_.py
+++ src/migrations/versions/ac95ebfa16cb_.py
@@ -6,9 +6,6 @@ Create Date: 2025-05-28 20:15:02.581903
 
 """
 
-import sqlalchemy as sa
-from alembic import op
-
 # revision identifiers, used by Alembic.
 revision = "ac95ebfa16cb"
 down_revision = ("4a87188db505", "fc708616c38b")
