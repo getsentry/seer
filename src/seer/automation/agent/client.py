f518cf0cd7f92dd88a65b71ba37807831142ebb8
Kush Dubey <kush.dubey@sentry.io>
2025-05-30 14:56:53 -0700

    fix(issue-summary): Use global endpoint

--- src/seer/automation/agent/client.py
+++ src/seer/automation/agent/client.py
@@ -974,15 +974,20 @@ class GeminiProvider:
             # "gemini-2.5-pro-preview-03-25",
         ]
 
-        region = (
-            "europe-west1"
-            if app_config.SENTRY_REGION == "de"
-            else (
-                "global"
-                if self.model_name in supported_models_on_global_endpoint and not use_local_endpoint
-                else "us-central1"
+        if self.model_name == "gemini-2.0-flash-lite-001":
+            region = "global"
+        else:
+            region = (
+                "europe-west1"
+                if app_config.SENTRY_REGION == "de"
+                else (
+                    "global"
+                    if self.model_name in supported_models_on_global_endpoint
+                    and not use_local_endpoint
+                    else "us-central1"
+                )
             )
-        )
+
         client = genai.Client(
             vertexai=True,
             location=region,
